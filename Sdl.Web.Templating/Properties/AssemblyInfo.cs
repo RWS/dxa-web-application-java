7bd763b5e0320e2d9c5db624db5f824e1f430de7
Bart Koopman <bkoopman@sdl.com>
2014-08-14 12:10:00 +0200

    added auto increase of build number for templating dll and removed administrator from tcmassemblyupload, so it will use current user credentials. we might need to change that back when we configure an auto build in jenkins

--- Sdl.Web.Templating/Properties/AssemblyInfo.cs
+++ Sdl.Web.Templating/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Tridion.ContentManager.Templating.Assembly;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -9,9 +8,9 @@ using Tridion.ContentManager.Templating.Assembly;
 [assembly: AssemblyTitle("Sdl.Web.Tridion.Templates")]
 [assembly: AssemblyDescription("SDL Tridion Reference Implementation Template Building Blocks")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SDL")]
-[assembly: AssemblyProduct("SDL Tridion Reference Implementation")]
-[assembly: AssemblyCopyright("Copyright ©  2000-2014 SDL Tridion Development Lab B.V.")]
+[assembly: AssemblyCompany("SDL Tridion B.V.")]
+[assembly: AssemblyProduct("SDL Web Reference Implementation")]
+[assembly: AssemblyCopyright("Copyright © 2014 SDL Tridion Development Lab B.V.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,8 +29,7 @@ using Tridion.ContentManager.Templating.Assembly;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// version information set in VersionInfo.cs so it can be updated by build server 

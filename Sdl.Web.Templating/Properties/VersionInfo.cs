793729a347db6082b848ad13d032b6492db0e4d0
Bart Koopman <bkoopman@sdl.com>
2014-09-05 10:07:59 +0200

    added assembly informational and file version to be set at build time and disabled postbuild step to upload. Assembly upload will be done by Jenkins now so we can have a separate server configured in each Jenkins build task

--- Sdl.Web.Templating/Properties/VersionInfo.cs
+++ Sdl.Web.Templating/Properties/VersionInfo.cs
@@ -1,1 +1,3 @@
 ﻿[assembly: System.Reflection.AssemblyVersion("7.1.*")]
+//[assembly: System.Reflection.AssemblyInformationalVersion("7.1.0.0")]
+//[assembly: System.Reflection.AssemblyFileVersion("7.1.0.0")]

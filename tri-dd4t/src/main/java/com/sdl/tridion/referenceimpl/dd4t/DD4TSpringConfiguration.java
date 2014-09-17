92fe3e8ec5535e3df674e3b8a810bd52dbab3dcf
Jesper de Jong <jdejong@sdl.com>
2014-09-17 15:48:04 +0200

    Moved DD4T-specific Spring configuration to module tri-dd4t, so that tri-webapp does not contain anything specific to DD4T anymore.

---
+++ tri-dd4t/src/main/java/com/sdl/tridion/referenceimpl/dd4t/DD4TSpringConfiguration.java
@@ -0,0 +1,9 @@
+package com.sdl.tridion.referenceimpl.dd4t;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:/tri-dd4t-config.xml")
+public class DD4TSpringConfiguration {
+}

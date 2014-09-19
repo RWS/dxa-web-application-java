32c28d53e81e8aed06763f05eefd9c0bec2ef533
Jesper de Jong <jdejong@sdl.com>
2014-09-19 14:14:28 +0200

    Small changes in the way Spring configuration is picked up from modules: dispatcher-servlet.xml now looks at all /META-INF/spring-context.xml files that can be found in the classpath.

--- tri-dd4t/src/main/java/com/sdl/tridion/referenceimpl/dd4t/DD4TSpringConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.sdl.tridion.referenceimpl.dd4t;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource("classpath:/tri-dd4t-config.xml")
-public class DD4TSpringConfiguration {
-}

fd3babee85088e295825f4e3463035912b5ed314
Jesper de Jong <jdejong@sdl.com>
2014-09-22 09:27:17 +0200

    BinaryFilter: Minor refactoring to make code more clear.

--- tri-webapp/src/main/java/com/sdl/tridion/referenceimpl/filter/BinaryFilter.java
+++ tri-webapp/src/main/java/com/sdl/tridion/referenceimpl/filter/BinaryFilter.java
@@ -34,32 +34,25 @@ public class BinaryFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         final HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         final String url = URLDecoder.decode(httpServletRequest.getRequestURI().replace(httpServletRequest.getContextPath(), ""), "UTF-8");
-        if (!url.endsWith(".html")) {
+        if (!url.endsWith(".html") && !url.endsWith(".jsp")) {
             try {
                 if (!handleRequest(url, response)) {
-                    LOG.debug("Continuing handler chain");
                     chain.doFilter(request, response);
                 }
             } catch (StorageException e) {
                 throw new ServletException("Error while handling request: " + url, e);
             }
+        } else {
+            chain.doFilter(request, response);
         }
     }
 
     private boolean handleRequest(String url, ServletResponse response) throws StorageException, IOException {
         LOG.debug("handleRequest: {}", url);
 
-        final List<BinaryVariant> binaryVariants = ((BinaryVariantDAO) StorageManagerFactory.getDAO(PUBLICATION_ID,
-                StorageTypeMapping.BINARY_VARIANT)).findByURL(url);
-        if (binaryVariants == null || binaryVariants.isEmpty()) {
-            LOG.debug("No binary variants found for: {}", url);
-            return false;
-        }
-
-        final BinaryVariant binaryVariant = binaryVariants.get(0);
+        final BinaryVariant binaryVariant = findBinaryVariant(PUBLICATION_ID, url);
         final BinaryMeta binaryMeta = binaryVariant.getBinaryMeta();
-        final ItemMeta itemMeta = ((ItemDAO) StorageManagerFactory.getDAO(PUBLICATION_ID, StorageTypeMapping.ITEM_META))
-                .findByPrimaryKey(binaryMeta.getPublicationId(), binaryMeta.getItemId());
+        final ItemMeta itemMeta = findItemMeta(binaryMeta.getPublicationId(), binaryMeta.getItemId());
 
         final File file = new File(servletContext.getRealPath(url));
 
@@ -76,9 +69,8 @@ public class BinaryFilter implements Filter {
         }
 
         if (refresh) {
-            final BinaryContent binaryContent = ((BinaryContentDAO) StorageManagerFactory.getDAO(PUBLICATION_ID,
-                    StorageTypeMapping.BINARY_CONTENT)).findByPrimaryKey(itemMeta.getPublicationId(),
-                    itemMeta.getItemId(), binaryVariant.getBinaryVariantId().getVariantId());
+            final BinaryContent binaryContent = findBinaryContent(itemMeta.getPublicationId(), itemMeta.getItemId(),
+                    binaryVariant.getVariantId());
 
             LOG.debug("Writing binary content to file: {}", file);
             Files.write(file.toPath(), binaryContent.getContent());
@@ -89,6 +81,29 @@ public class BinaryFilter implements Filter {
         LOG.debug("Sending response with content of file: {}", file);
         Files.copy(file.toPath(), response.getOutputStream());
         return true;
+    }
+
+    private BinaryVariant findBinaryVariant(int publicationId, String url) throws StorageException {
+        final BinaryVariantDAO dao = (BinaryVariantDAO) StorageManagerFactory.getDAO(publicationId,
+                StorageTypeMapping.BINARY_VARIANT);
+        final List<BinaryVariant> binaryVariants = dao.findByURL(url);
+        if (binaryVariants == null || binaryVariants.isEmpty()) {
+            LOG.debug("No binary variants found for: {}", url);
+            return null;
+        }
+
+        return binaryVariants.get(0);
+    }
+
+    private ItemMeta findItemMeta(int publicationId, int itemId) throws StorageException {
+        final ItemDAO dao = (ItemDAO) StorageManagerFactory.getDAO(PUBLICATION_ID, StorageTypeMapping.ITEM_META);
+        return dao.findByPrimaryKey(publicationId, itemId);
+    }
+
+    private BinaryContent findBinaryContent(int publicationId, int itemId, String variantId) throws StorageException {
+        final BinaryContentDAO dao = (BinaryContentDAO) StorageManagerFactory.getDAO(PUBLICATION_ID,
+                StorageTypeMapping.BINARY_CONTENT);
+        return dao.findByPrimaryKey(publicationId, itemId, variantId);
     }
 
     @Override

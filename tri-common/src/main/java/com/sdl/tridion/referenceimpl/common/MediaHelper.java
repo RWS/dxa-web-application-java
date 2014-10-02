07f85b1429c39969645501d8e192ee4c88ff1992
Jesper de Jong <jdejong@sdl.com>
2014-10-02 10:52:06 +0200

    Implemented MediaHelper, BaseMediaHelper, ContextualMediaHelper and the <tri:image> tag. Image resizing with CID now works.

--- tri-common/src/main/java/com/sdl/tridion/referenceimpl/common/MediaHelper.java
+++ tri-common/src/main/java/com/sdl/tridion/referenceimpl/common/MediaHelper.java
@@ -1,12 +1,19 @@
 package com.sdl.tridion.referenceimpl.common;
 
+/**
+ * TODO: Documentation.
+ */
 public interface MediaHelper {
-
-    // TODO: See IMediaHelper (C#); implement ContextualMediaHelper
 
     int getResponsiveWidth(String widthFactor, int containerSize);
 
     int getResponsiveHeight(String widthFactor, double aspect, int containerSize);
 
     String getResponsiveImageUrl(String url, String widthFactor, double aspect, int containerSize);
+
+    int getGridSize();
+
+    double getDefaultMediaAspect();
+
+    String getDefaultMediaFill();
 }

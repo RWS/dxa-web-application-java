ff649525d1ed35e964be7260fb5a9893f9ca7052
Jesper de Jong <jdejong@sdl.com>
2014-10-07 14:01:31 +0200

    Cache Localization objects.

--- tri-common/src/main/java/com/sdl/tridion/referenceimpl/common/config/TridionConfiguration.java
+++ tri-common/src/main/java/com/sdl/tridion/referenceimpl/common/config/TridionConfiguration.java
@@ -6,25 +6,42 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class TridionConfiguration {
     private static final Logger LOG = LoggerFactory.getLogger(TridionConfiguration.class);
 
-    public Localization getLocalization(String url) {
-        // TODO: Add caching mechanism so that we don't have to call the CD API and create a new Localization
-        // for every request
+    private Map<Integer, Localization> localizations = new HashMap<>();
 
+    public Localization getLocalization(String url) {
         final PublicationMapping publicationMapping = DynamicContent.getInstance().getMappingsResolver()
                 .getPublicationMappingFromUrl(url);
 
         if (publicationMapping != null) {
-            return Localization.newBuilder()
-                    .setLocalizationId(publicationMapping.getPublicationId())
-                    .setProtocol(publicationMapping.getProtocol())
-                    .setDomain(publicationMapping.getDomain())
-                    .setPort(publicationMapping.getPort())
-                    .setPath(publicationMapping.getPath())
-                    .build();
+            final int publicationId = publicationMapping.getPublicationId();
+
+            Localization localization;
+            synchronized (localizations) {
+                localization = localizations.get(publicationId);
+                if (localization == null) {
+                    localization = Localization.newBuilder()
+                            .setLocalizationId(publicationId)
+                            .setProtocol(publicationMapping.getProtocol())
+                            .setDomain(publicationMapping.getDomain())
+                            .setPort(publicationMapping.getPort())
+                            .setPath(publicationMapping.getPath())
+                            .build();
+
+                    LOG.debug("Created: {} -> {}", url, localization);
+                    localizations.put(publicationId, localization);
+                } else {
+                    LOG.debug("Returning cached: {} -> {}", url, localization);
+                }
+            }
+
+            return localization;
         }
 
         return null;

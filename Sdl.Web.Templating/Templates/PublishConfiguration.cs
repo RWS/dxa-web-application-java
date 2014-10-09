b2dc629ce12b924dca8000f853f07cce775787f6
Will Price <wprice@sdl.com>
2014-10-09 14:19:15 +0200

    Put publication id only into siteLocalizations array

--- Sdl.Web.Templating/Templates/PublishConfiguration.cs
+++ Sdl.Web.Templating/Templates/PublishConfiguration.cs
@@ -160,7 +160,8 @@ namespace Sdl.Web.Tridion.Templates
                 var filter = new UsingItemsFilter(Engine.GetSession()) { ItemTypes = new List<ItemType> { ItemType.Publication } };
                 foreach (XmlElement item in GetPublication().GetListUsingItems(filter).ChildNodes)
                 {
-                    pubIds.Add(item.GetAttribute("ID"));
+                    var id = new TcmUri(item.GetAttribute("ID"));
+                    pubIds.Add(id.ItemId.ToString());
                 }
             }
             else

4462abd63bd74a359712c085d6d37f01775dfe64
Jesper de Jong <jdejong@sdl.com>
2014-10-24 16:45:26 +0200

    Added method for debugging that returns page in JSON format.

--- webapp-main/src/main/java/com/sdl/webapp/main/controller/core/PageController.java
+++ webapp-main/src/main/java/com/sdl/webapp/main/controller/core/PageController.java
@@ -1,6 +1,6 @@
 package com.sdl.webapp.main.controller.core;
 
-import com.sdl.webapp.common.api.*;
+import com.sdl.webapp.common.api.WebRequestContext;
 import com.sdl.webapp.common.api.content.ContentProvider;
 import com.sdl.webapp.common.api.content.ContentProviderException;
 import com.sdl.webapp.common.api.content.PageNotFoundException;
@@ -11,12 +11,19 @@ import com.sdl.webapp.main.controller.exception.NotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.util.UrlPathHelper;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import static com.sdl.webapp.main.RequestAttributeNames.PAGE_MODEL;
 import static com.sdl.webapp.main.RequestAttributeNames.SCREEN_WIDTH;
@@ -24,6 +31,8 @@ import static com.sdl.webapp.main.RequestAttributeNames.SCREEN_WIDTH;
 @Controller
 public class PageController extends AbstractController {
     private static final Logger LOG = LoggerFactory.getLogger(PageController.class);
+
+    private static final int BUFFER_SIZE = 8192;
 
     private final UrlPathHelper urlPathHelper = new UrlPathHelper();
 
@@ -33,13 +42,13 @@ public class PageController extends AbstractController {
     @Autowired
     private WebRequestContext webRequestContext;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/**")
+    @RequestMapping(method = RequestMethod.GET, value = "/**", produces = { "text/html" })
     public String handleGetPage(HttpServletRequest request) {
         // Strip the protocol, domain, port and context path off of the URL
         final String url = urlPathHelper.getRequestUri(request).replace(urlPathHelper.getContextPath(request), "");
         LOG.trace("handleGetPage: url={}", url);
 
-        final Page page = getPageFromContentProvider(url, webRequestContext.getLocalization());
+        final Page page = getPageModel(url, webRequestContext.getLocalization());
         LOG.trace("handleGetPage: page={}", page);
 
         request.setAttribute(PAGE_MODEL, page);
@@ -50,9 +59,42 @@ public class PageController extends AbstractController {
         return viewName;
     }
 
-    private Page getPageFromContentProvider(String url, Localization localization) {
+    // TODO: This is only for debugging and should not be enabled
+    @RequestMapping(method = RequestMethod.GET, value = "/**", produces = { "application/json" })
+    public void handleGetPageJSON(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Strip the protocol, domain, port, context path and prefix off of the URL
+        final String url = urlPathHelper.getRequestUri(request).replace(urlPathHelper.getContextPath(request), "");
+        LOG.trace("handleGetPageJSON: url={}", url);
+
+        final ServletServerHttpResponse res = new ServletServerHttpResponse(response);
+        res.setStatusCode(HttpStatus.OK);
+        res.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+
+        try (final InputStream in = getPageContent(url, webRequestContext.getLocalization());
+             final OutputStream out = res.getBody()) {
+            final byte[] buffer = new byte[BUFFER_SIZE];
+            int count;
+            while ((count = in.read(buffer)) > 0) {
+                out.write(buffer, 0, count);
+            }
+        }
+    }
+
+    private Page getPageModel(String url, Localization localization) {
         try {
             return contentProvider.getPageModel(url, localization);
+        } catch (PageNotFoundException e) {
+            LOG.error("Page not found: {}", url, e);
+            throw new NotFoundException("Page not found: " + url, e);
+        } catch (ContentProviderException e) {
+            LOG.error("An unexpected error occurred", e);
+            throw new InternalServerErrorException("An unexpected error occurred", e);
+        }
+    }
+
+    private InputStream getPageContent(String url, Localization localization) {
+        try {
+            return contentProvider.getPageContent(url, localization);
         } catch (PageNotFoundException e) {
             LOG.error("Page not found: {}", url, e);
             throw new NotFoundException("Page not found: " + url, e);

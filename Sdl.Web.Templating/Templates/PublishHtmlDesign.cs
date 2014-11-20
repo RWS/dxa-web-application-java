4d5c9af29a68bb9e530ed262dbeeeee9cd16df98
Bart Koopman <bkoopman@sdl.com>
2014-11-20 16:21:03 +0100

    TSI-676 fixed possible incorrect content type determination

--- Sdl.Web.Templating/Templates/PublishHtmlDesign.cs
+++ Sdl.Web.Templating/Templates/PublishHtmlDesign.cs
@@ -319,6 +319,12 @@ namespace Sdl.Web.Tridion.Templates
 
         private static ContentType GetContentType(string extension)
         {
+            // remove dot if extension starts with it
+            if (extension.StartsWith("."))
+            {
+                extension = extension.Substring(1);
+            }
+
             switch (extension)
             {
                 case "css":

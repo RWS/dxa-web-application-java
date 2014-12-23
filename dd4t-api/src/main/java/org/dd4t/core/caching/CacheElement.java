a51bd42e86bd73b63aa5b5724b0ca6e04933e91e
Raimond Kempees <kempees@radagio.com>
2014-12-23 17:34:10 +0100

    test

--- dd4t-api/src/main/java/org/dd4t/core/caching/CacheElement.java
+++ dd4t-api/src/main/java/org/dd4t/core/caching/CacheElement.java
@@ -4,7 +4,7 @@ package org.dd4t.core.caching;
  * Wrapper around a 'payload' object that represents the actual object in the cache.
  * The payload is wrapped inside this cache element object,
  * so we can differentiate between null and not-null payloads.
- * The cache element can also contain empty (null) payloads.
+ * The cache element can also contain empty (null)  payloads.
  *
  * @author Mihai Cadariu
  */

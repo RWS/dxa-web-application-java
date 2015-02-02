fbbdb503488bfbb4aed1606accb346ce9722c080
Raimond Kempees <raimond.kempees@tomtom.com>
2015-02-02 18:31:34 +0100

    Removed LabelService

--- dd4t-api/src/main/java/org/dd4t/core/factories/TaxonomyServiceFactory.java
+++ dd4t-api/src/main/java/org/dd4t/core/factories/TaxonomyServiceFactory.java
@@ -5,9 +5,6 @@ import org.dd4t.core.services.TaxonomyService;
 import java.io.IOException;
 
 /**
- *
- * TODO: should this be in the API?
- *
  * @author Mihai Cadariu
  */
 public interface TaxonomyServiceFactory {
@@ -15,19 +12,19 @@ public interface TaxonomyServiceFactory {
     /**
      * Generic taxonomy service.
      *
-     * @param category String representing the Tridion Category XML name or TCMURI
+     * @param categoryURI String representing the Tridion Category XML name or TCMURI
      * @return TaxonomyService representing taxonomy service
      * @throws IOException if something goes wrong while fetching the taxonomy
      */
-    public TaxonomyService getTaxonomyService(String category) throws IOException;
+    public TaxonomyService getTaxonomyService(String categoryURI) throws IOException;
 
     /**
      * Generic taxonomy service that retrieves only classified Component based on the given SchemaURI.
      *
-     * @param category String representing the Tridion Category XML name or TCMURI
-     * @param schema   String representing the filter for classified related Components to return for each Keyword
+     * @param categoryURI String representing the Tridion Category XML name or TCMURI
+     * @param schemaURI   String representing the filter for classified related Components to return for each Keyword
      * @return TaxonomyService
      * @throws IOException if something goes wrong while fetching the taxonomy
      */
-    public TaxonomyService getTaxonomyBySchemaService(String category, String schema) throws IOException;
+    public TaxonomyService getTaxonomyBySchemaService(String categoryURI, String schemaURI) throws IOException;
 }

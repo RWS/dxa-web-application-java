7d53b79fc2b89ca311d6a033e442b11960ef83db
Bart Koopman <bkoopman@sdl.com>
2015-07-21 16:51:09 +0200

    TSI-112 installed ECL and Media Manager connector 1.3 on saintjohn01.ams.dev and created a new TBB which we should test now (separate Templating solution because it needs to be part of a module, which means we might need to copy this code to the dxa-modules repository, but I need to discuss that with Rick) also this TBB only works when there is a Direct Link available for the ECL item (which is the case for Media Manager), if that is not there, we should use the Content and publish the binary and resolve the URL to that, which is why this should probably all be merged in the DD4T 'Publish binaries for component' TBB (and what about the 'Publish binaries for page' TBB) also I'm not doing anything with External Metadata yet...

---
+++ Sdl.Web.Templating.Ecl/Common/ItemFieldsExtensions.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tridion.ContentManager.ContentManagement;
+using Tridion.ContentManager.ContentManagement.Fields;
+
+namespace Sdl.Web.Tridion.Common
+{
+    public static class ItemFieldsExtensions
+    {
+        public static double GetNumberValue(this ItemFields fields, string fieldName)
+        {
+            return fields.GetNumberValues(fieldName).FirstOrDefault();
+        }
+
+        public static IEnumerable<double> GetNumberValues(this ItemFields fields, string fieldName)
+        {
+            return
+                null != fields && fields.Contains(fieldName)
+                    ? (fields[fieldName] as NumberField).Values
+                    : new double[0];
+        }
+
+        public static Keyword GetKeywordValue(this ItemFields fields, string fieldName)
+        {
+            return fields.GetKeywordValues(fieldName).FirstOrDefault();
+        }
+
+        public static IEnumerable<Keyword> GetKeywordValues(this ItemFields fields, string fieldName)
+        {
+            return
+                null != fields && fields.Contains(fieldName)
+                    ? (fields[fieldName] as KeywordField).Values
+                    : Enumerable.Empty<Keyword>();
+        }
+
+        public static Component GetComponentValue(this ItemFields fields, string fieldName)
+        {
+            return fields.GetComponentValues(fieldName).FirstOrDefault();
+        }
+
+        public static IEnumerable<Component> GetComponentValues(this ItemFields fields, string fieldName)
+        {
+            return
+                null != fields && fields.Contains(fieldName)
+                    ? (fields[fieldName] as ComponentLinkField).Values
+                    : Enumerable.Empty<Component>();
+        }
+
+        public static string GetExternalLink(this ItemFields fields, string fieldName)
+        {
+            return fields.GetExternalLinks(fieldName).FirstOrDefault() ?? string.Empty;
+        }
+
+        public static IEnumerable<string> GetExternalLinks(this ItemFields fields, string fieldName)
+        {
+            return
+                null != fields && fields.Contains(fieldName)
+                    ? (fields[fieldName] as ExternalLinkField).Values
+                    : new string[0];
+        }
+
+        public static Component GetMultimediaLink(this ItemFields fields, string fieldName)
+        {
+            return fields.GetMultimediaLinks(fieldName).FirstOrDefault();
+        }
+
+        public static IEnumerable<Component> GetMultimediaLinks(this ItemFields fields, string fieldName)
+        {
+            return
+                null != fields && fields.Contains(fieldName)
+                    ? (fields[fieldName] as MultimediaLinkField).Values
+                    : Enumerable.Empty<Component>();
+        }
+
+        public static ItemFields GetEmbeddedField(this ItemFields fields, string fieldName)
+        {
+            return fields.GetEmbeddedFields(fieldName).FirstOrDefault();
+        }
+
+        public static IEnumerable<ItemFields> GetEmbeddedFields(this ItemFields fields, string fieldName)
+        {
+            return
+                null != fields && fields.Contains(fieldName)
+                    ? (fields[fieldName] as EmbeddedSchemaField).Values
+                    : Enumerable.Empty<ItemFields>();
+        }
+
+        public static string GetTextValue(this ItemFields fields, string fieldName)
+        {
+            return GetTextValues(fields, fieldName).FirstOrDefault() ?? string.Empty;
+        }
+
+        public static IEnumerable<string> GetTextValues(this ItemFields fields, string fieldName)
+        {
+            return
+                null != fields && fields.Contains(fieldName)
+                    ? (fields[fieldName] as TextField).Values
+                    : new string[0];
+        }
+
+        public static DateTime? GetDateValue(this ItemFields fields, string fieldName = "date")
+        {
+            return fields.GetDateValues(fieldName).FirstOrDefault();
+        }
+
+        public static IEnumerable<DateTime?> GetDateValues(this ItemFields fields, string fieldName = "date")
+        {
+            return
+                null != fields && fields.Contains(fieldName)
+                    ? (fields[fieldName] as DateField).Values.Select(d => d == DateTime.MinValue ? null : (DateTime?)d)
+                    : new DateTime?[0];
+        }
+
+        public static int GetFieldValueCount(this ItemFields fields, string fieldName)
+        {
+            if (null == fields)
+            {
+                return 0;
+            }
+
+            ItemField field = fields[fieldName];
+
+            return
+                field is ComponentLinkField
+                    ? (field as ComponentLinkField).Values.Count
+                    : field is TextField
+                        ? (field as TextField).Values.Count
+                        : field is EmbeddedSchemaField
+                            ? (field as EmbeddedSchemaField).Values.Count
+                            : 0;
+        }
+
+        /// <summary>
+        /// Manual unification of different field types logic to overcome native tridion implementation shortcoming,
+        /// which is not polymorphic.
+        /// </summary>
+        static readonly IDictionary<Type, Func<ItemFields, string, string>> ValueResolver =
+            new Dictionary<Type, Func<ItemFields, string, string>> {
+            { typeof(KeywordField), (fields, name) => fields.GetKeywordValues(name).Select(k => k.Title).FirstOrDefault() },
+            { typeof(ComponentLinkField), (fields, name) => fields.GetComponentValues(name).Select(c => c.Id).FirstOrDefault() },
+            { typeof(ExternalLinkField), (fields, name) => fields.GetExternalLinks(name).FirstOrDefault() },
+            { typeof(MultimediaLinkField), (fields, name) => fields.GetMultimediaLinks(name).Select(mc => mc.Title).FirstOrDefault() },
+            { typeof(DateField), (fields, name) => ((DateTime)fields.GetDateValues(name).FirstOrDefault()).ToString("yyyy-MM-dd HH:mm:ss") }
+        };
+
+        /// <summary>
+        /// Gets a sensible string represntation of a field.
+        /// </summary>
+        public static string GetSingleFieldValue(this ItemFields fields, string fieldName)
+        {
+            ItemField field;
+            Type fieldType;
+
+            return
+                null == fields
+                || !fields.Contains(fieldName)
+                    ? String.Empty
+                    : ValueResolver.ContainsKey((fieldType = (field = fields[fieldName]).GetType()))
+                        ? ValueResolver[fieldType](fields, fieldName) ?? String.Empty
+                        : field.ToString();
+        }
+    }
+}
---
+++ Sdl.Web.Templating.Ecl/Templates/ResolveEclItems.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Xml;
+using Sdl.Web.Tridion.Common;
+using Tridion.ContentManager.ContentManagement;
+using Tridion.ContentManager.Templating;
+using Tridion.ContentManager.Templating.Assembly;
+using Tridion.ExternalContentLibrary.Templating.V2;
+using Newtonsoft.Json;
+
+namespace Sdl.Web.Tridion.Templates
+{
+    /// <summary>
+    /// Resolves ECL items from the package output.
+    /// Update URL tags with the External URL if available so these can be used directly by the web application. 
+    /// </summary>
+    /// <remarks>
+    /// Should be placed after the "Publish binaries for component" TBB
+    /// </remarks>
+    [TcmTemplateTitle("Resolve External Content Library Items")]
+    [TcmTemplateParameterSchema("resource:Sdl.Web.Tridion.Resources.ResolveEclItemsParameters.xsd")]
+    public class ResolveEclItems : TemplateBase
+    {
+        private ExternalContentLibraryFunctionSource _eclFunctions;
+
+        protected void Init(Engine engine, Package package)
+        {
+            Initialize(engine, package);
+            _eclFunctions = new ExternalContentLibraryFunctionSource();
+            _eclFunctions.Initialize(engine, package);
+        }
+
+        public override void Transform(Engine engine, Package package)
+        {
+            Initialize(engine, package);
+            Component comp = GetComponent();
+            if (IsPageTemplate() || comp == null)
+            {
+                Logger.Error("No Component found (is this a Page Template?)");
+                return;
+            }
+
+            Item outputItem = package.GetByName(Package.OutputName);
+            if (outputItem == null)
+            {
+                Logger.Error("No Output package item found (is this TBB placed at the end?)");
+                return;
+            }
+            string output = outputItem.GetAsString();
+            if (outputItem.ContentType.Equals(ContentType.Xml))
+            {
+                Logger.Debug("Content is XML");
+                //XML - only for backwards compatibility
+                XmlDocument xmlDocument;
+                if (ResolveXmlContent(output, out xmlDocument))
+                {
+                    package.Remove(outputItem);
+                    package.PushItem(Package.OutputName, package.CreateXmlDocumentItem(ContentType.Xml, xmlDocument));
+                }
+            }
+            else
+            {
+                Logger.Debug("Content is JSON");
+                //JSON
+                string json;
+                if (ResolveJsonContent(output, out json))
+                {
+                    package.Remove(outputItem);
+                    package.PushItem(Package.OutputName, package.CreateStringItem(ContentType.Text, json));
+                }
+            }
+        }
+
+        private bool ResolveJsonContent(string content, out string json)
+        {
+            json = content;
+
+            XmlDocument xmlDocument = JsonConvert.DeserializeXmlNode(json, "Component", true);
+            bool containsEclReferences = ResolveContentDocument(xmlDocument);
+            if (containsEclReferences)
+            {
+                json = JsonConvert.SerializeXmlNode(xmlDocument, Newtonsoft.Json.Formatting.None, true);
+            }
+
+            return containsEclReferences;
+        }
+
+        private bool ResolveXmlContent(string content, out XmlDocument xmlDocument)
+        {
+            xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(content);
+
+            return ResolveContentDocument(xmlDocument);
+        }
+
+        private bool ResolveContentDocument(XmlDocument xmlDocument)
+        {
+            bool containsEclReferences = false;
+
+            XmlNodeList multimediaComponentElements = xmlDocument.SelectNodes("//Multimedia[MimeType='application/externalcontentlibrary']/..");
+
+            Logger.Debug(String.Format("Resolving {0} External Content Library reference(s)", multimediaComponentElements.Count));
+
+            foreach (XmlElement multimediaComponentElement in multimediaComponentElements)
+            {
+                ResolveEclReference(multimediaComponentElement);
+                containsEclReferences = true;
+            }
+
+            return containsEclReferences;
+        }
+
+        private void ResolveEclReference(XmlElement multimediaComponentElement)
+        {
+            Logger.Debug(String.Format("Multimedia Component XML [{0}]", multimediaComponentElement.OuterXml));
+
+            string id = multimediaComponentElement.SelectSingleNode("Id").IfNotNull(i => i.InnerText);
+            if (!String.IsNullOrEmpty(id))
+            {
+                XmlNode urlNode = multimediaComponentElement.SelectSingleNode("Multimedia/Url");
+                if (urlNode != null)
+                {
+                    if (_eclFunctions.IsExternalContentLibraryComponent(id))
+                    {
+                        // TODO: should we consider using the template fragment somehow?
+                        // TODO: direct link could be null, then content is available and we should add a binary to the package and publish that
+                        // in case of the latter, shouldn't this be handled in the DD4T Publish binaries for component TBB? 
+                        urlNode.InnerText = _eclFunctions.GetExternalContentLibraryDirectLink(id);
+                    }
+                }
+            }
+        }
+    }
+}

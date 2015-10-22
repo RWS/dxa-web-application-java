45178ec885cb71bdcdedfdc9a38c99a32638500c
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2015-10-22 10:50:10 +0200

    removed unused util class

--- common-impl/src/main/java/com/sdl/webapp/common/impl/interceptor/StaticContentInterceptor.java
+++ common-impl/src/main/java/com/sdl/webapp/common/impl/interceptor/StaticContentInterceptor.java
@@ -6,7 +6,6 @@ import com.sdl.webapp.common.api.content.StaticContentItem;
 import com.sdl.webapp.common.api.content.StaticContentNotFoundException;
 import com.sdl.webapp.common.api.localization.Localization;
 import com.sdl.webapp.common.util.MimeUtils;
-import com.sdl.webapp.common.util.StreamUtils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;

9e9ee5e506059b6f34d81a707e55bc8093a30728
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2015-10-30 16:47:46 +0100

    TSI-1010 DXA maven layout

---
+++ dxa-framework/dxa-common-impl/src/main/java/com/sdl/webapp/common/impl/mapping/SemanticMappingRegistryFactory.java
@@ -0,0 +1,30 @@
+package com.sdl.webapp.common.impl.mapping;
+
+import com.sdl.webapp.common.api.mapping.SemanticMappingRegistry;
+import com.sdl.webapp.common.api.model.entity.AbstractEntityModel;
+import org.springframework.beans.factory.config.AbstractFactoryBean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+/**
+ * Spring factory bean to create the {@code SemanticMappingRegistry} bean.
+ */
+@Component
+@Primary
+public class SemanticMappingRegistryFactory extends AbstractFactoryBean<SemanticMappingRegistry> {
+
+    @Override
+    public Class<?> getObjectType() {
+        return SemanticMappingRegistry.class;
+    }
+
+    @Override
+    protected SemanticMappingRegistry createInstance() throws Exception {
+        SemanticMappingRegistryImpl semanticMappingRegistry = new SemanticMappingRegistryImpl();
+
+        // Register all entity classes in the same package (and subpackages) as AbstractEntity
+        semanticMappingRegistry.registerEntities(AbstractEntityModel.class.getPackage().getName());
+
+        return semanticMappingRegistry;
+    }
+}

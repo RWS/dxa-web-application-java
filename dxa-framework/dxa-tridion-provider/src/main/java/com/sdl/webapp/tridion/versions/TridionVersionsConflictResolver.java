f0973f6ca86d1bf0b468d72f64f709eeda2d8b2f
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2015-12-11 11:49:23 +0100

    TSI-1069 conflict resolver for 2013SP1/Web8

---
+++ dxa-framework/dxa-tridion-provider/src/main/java/com/sdl/webapp/tridion/versions/TridionVersionsConflictResolver.java
@@ -0,0 +1,10 @@
+package com.sdl.webapp.tridion.versions;
+
+/**
+ * This interface is introduced to resolve differences between Web 2013SP1 and Web8
+ * and should be removed once 2013SP1 support is dropped.
+ * @deprecated should be only used exceptionally, and removed ASAP
+ */
+public interface TridionVersionsConflictResolver {
+
+}
---
+++ dxa-framework/dxa-tridion-provider/src/main/java/com/sdl/webapp/tridion/versions/Web7Resolver.java
@@ -0,0 +1,8 @@
+package com.sdl.webapp.tridion.versions;
+
+/**
+ * @deprecated should be removed once 2013SP1 support is dropped
+ */
+public class Web7Resolver implements TridionVersionsConflictResolver {
+
+}

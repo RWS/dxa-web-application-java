1d030575cb1e1b72bd90f5d328062082f94d4e7c
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2015-12-11 13:55:34 +0100

    TSI-1069 conflict resolvers as separate projects

--- dxa-framework/dxa-tridion-provider/tridion-versions-resolver-web7/src/main/java/com/sdl/webapp/tridion/versions/Web7Resolver.java
+++ dxa-framework/dxa-tridion-provider/tridion-versions-resolver-web7/src/main/java/com/sdl/webapp/tridion/versions/Web7Resolver.java
@@ -1,8 +1,11 @@
 package com.sdl.webapp.tridion.versions;
 
+import org.springframework.stereotype.Service;
+
 /**
  * @deprecated should be removed once 2013SP1 support is dropped
  */
+@Service
 public class Web7Resolver implements TridionVersionsConflictResolver {
 
 }
--- dxa-framework/dxa-tridion-provider/tridion-versions-resolver-web8/src/main/java/com/sdl/webapp/tridion/versions/Web8Resolver.java
+++ dxa-framework/dxa-tridion-provider/tridion-versions-resolver-web8/src/main/java/com/sdl/webapp/tridion/versions/Web8Resolver.java
@@ -1,8 +1,11 @@
 package com.sdl.webapp.tridion.versions;
 
+import org.springframework.stereotype.Service;
+
 /**
  * @deprecated should be removed once 2013SP1 support is dropped
  */
+@Service
 public class Web8Resolver implements TridionVersionsConflictResolver {
 
 }

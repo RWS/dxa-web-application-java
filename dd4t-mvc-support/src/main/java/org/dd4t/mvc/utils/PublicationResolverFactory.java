6c194518e2edf4e5f639725ab9b0046eea180fb0
Raimond Kempees <kempees@radagio.com>
2015-12-21 13:29:10 +0100

    Simplified the PublicationResolver

--- dd4t-mvc-support/src/main/java/org/dd4t/mvc/utils/PublicationResolverFactory.java
+++ dd4t-mvc-support/src/main/java/org/dd4t/mvc/utils/PublicationResolverFactory.java
@@ -16,7 +16,6 @@
 
 package org.dd4t.mvc.utils;
 
-import org.dd4t.core.factories.PublicationResolverFactory;
 import org.dd4t.core.resolvers.PublicationResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,34 +23,33 @@ import org.slf4j.LoggerFactory;
 /**
  * PublicationResolverFactoryImpl hides actual implementation
  * Configured through Spring beans
- *
+ * <p/>
  * This is needed for allowing static access in PublicationUrl.java,
  * an EL function.
  *
  * @author Mihai Cadariu
  */
-public class PublicationResolverFactoryImpl implements PublicationResolverFactory {
+public class PublicationResolverFactory {
 
-    private static final Logger LOG = LoggerFactory.getLogger(PublicationResolverFactoryImpl.class);
-    private static final PublicationResolverFactory INSTANCE = new PublicationResolverFactoryImpl();
+	private static final Logger LOG = LoggerFactory.getLogger(PublicationResolverFactory.class);
+	private static final PublicationResolverFactory INSTANCE = new PublicationResolverFactory();
 
 	private PublicationResolver publicationResolver;
 
-    private PublicationResolverFactoryImpl() {
-        LOG.debug("Create new instance");
-    }
+	private PublicationResolverFactory () {
 
-    public static PublicationResolverFactory getInstance() {
-        return INSTANCE;
-    }
+	}
 
-    @Override
-    public PublicationResolver getPublicationResolver() {
-        return publicationResolver;
-    }
+	public static PublicationResolverFactory getInstance () {
+		return INSTANCE;
+	}
 
-    public void setPublicationResolver(PublicationResolver publicationResolver) {
-        LOG.debug("Set PublicationResolver " + publicationResolver);
-        this.publicationResolver = publicationResolver;
-    }
+	public static PublicationResolver getPublicationResolver () {
+		return INSTANCE.publicationResolver;
+	}
+
+	public void setPublicationResolver (PublicationResolver publicationResolver) {
+		LOG.debug("Set PublicationResolver " + publicationResolver);
+		this.publicationResolver = publicationResolver;
+	}
 }

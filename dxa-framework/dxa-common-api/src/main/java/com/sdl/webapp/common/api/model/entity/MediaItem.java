55ecf14e98868b41db9323990496ad95acd8700f
Jarno Henneman <jhenneman@sdl.com>
2016-06-23 16:53:23 +0200

    so ?

--- dxa-framework/dxa-common-api/src/main/java/com/sdl/webapp/common/api/model/entity/MediaItem.java
+++ dxa-framework/dxa-common-api/src/main/java/com/sdl/webapp/common/api/model/entity/MediaItem.java
@@ -164,13 +164,11 @@ public abstract class MediaItem extends AbstractEntityModel {
         // Return the Item (Reference) ID part of the TCM URI.
         this.setId(xhtmlElement.getAttributes().getNamedItem("xlink:href").getNodeValue().split("-")[1]);
         this.setUrl(xhtmlElement.getAttributes().getNamedItem("src").getNodeValue());
-        this.setHtmlClasses(xhtmlElement.getAttributes().getNamedItem("classes").getNodeValue());
         this.fileName = xhtmlElement.getAttributes().getNamedItem("data-multimediaFileName").getNodeValue();
         String fileSize = xhtmlElement.getAttributes().getNamedItem("data-multimediaFileSize").getNodeValue();
         if (!Strings.isNullOrEmpty(fileSize)) {
             this.fileSize = Integer.parseInt(fileSize);
         }
-
         this.mimeType = xhtmlElement.getAttributes().getNamedItem("data-multimediaMimeType").getNodeValue();
         this.isEmbedded = true;
     }

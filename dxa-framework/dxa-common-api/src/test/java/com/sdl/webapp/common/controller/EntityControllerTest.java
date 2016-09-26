dfee0c1a15947a775ccb88aaccafb5ffa776846b
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2016-09-26 13:08:08 +0200

    TSI-1928 test for RedirectEntity handling by EntityController

---
+++ dxa-framework/dxa-common-api/src/test/java/com/sdl/webapp/common/controller/EntityControllerTest.java
@@ -0,0 +1,33 @@
+package com.sdl.webapp.common.controller;
+
+import com.sdl.webapp.common.api.model.ViewModel;
+import com.sdl.webapp.common.api.model.entity.Link;
+import com.sdl.webapp.common.api.model.entity.RedirectEntity;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.junit.Assert.assertEquals;
+
+public class EntityControllerTest {
+
+    @Test
+    public void shouldReturnRedirectViewInCaseOfRedirectEntity() throws Exception {
+        //given 
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setAttribute("_entity_", new Link());
+
+        //when
+        String viewName = new EntityController() {
+            @Override
+            protected ViewModel enrichModel(ViewModel model, HttpServletRequest httpServletRequest) throws Exception {
+                return new RedirectEntity("path");
+            }
+        }.handleGetEntity(request, "id");
+
+        //then
+        assertEquals("RedirectView", viewName);
+    }
+
+}

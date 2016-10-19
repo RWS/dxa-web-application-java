969f4f0c7a9c67b33f611d4751ede742dd6ebabb
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2016-10-19 14:58:46 +0200

    TSI-1989 remember-me functionality

--- dxa-framework/dxa-tridion-provider/src/main/java/com/sdl/webapp/config/AbstractExternalSystemsFiltersWebConfiguration.java
+++ dxa-framework/dxa-tridion-provider/src/main/java/com/sdl/webapp/config/AbstractExternalSystemsFiltersWebConfiguration.java
@@ -1,6 +1,7 @@
 package com.sdl.webapp.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
 import org.springframework.web.WebApplicationInitializer;
 
 import javax.servlet.ServletContext;
@@ -9,7 +10,7 @@ import javax.servlet.ServletException;
 import static com.sdl.webapp.common.util.InitializationUtils.loadDxaProperties;
 
 @Slf4j
-public abstract class AbstractExternalSystemsFiltersWebConfiguration implements WebApplicationInitializer {
+public abstract class AbstractExternalSystemsFiltersWebConfiguration implements WebApplicationInitializer, Ordered {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         log.trace("Registration for XPM and preview filters if needed...");
@@ -22,6 +23,11 @@ public abstract class AbstractExternalSystemsFiltersWebConfiguration implements 
             log.debug("dxa.web.adf.enabled = true, thus registering ADF");
             registerAdf(servletContext);
         }
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
     }
 
     protected abstract void registerXpm(ServletContext servletContext);

5223999b58704cb7faf2e8d161a1d9d800f32d97
Raimond Kempees <kempees@radagio.com>
2016-10-20 09:22:31 +0200

    Moved the NoCacheProvider

---
+++ dd4t-caching/src/main/java/org/dd4t/core/providers/NoCacheProvider.java
@@ -0,0 +1,77 @@
+package org.dd4t.core.providers;
+
+import org.dd4t.core.caching.Cachable;
+import org.dd4t.core.caching.CacheElement;
+import org.dd4t.core.caching.CacheInvalidator;
+import org.dd4t.providers.CacheProvider;
+import org.dd4t.providers.PayloadCacheProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+
+/**
+ * dd4t-parent
+ *
+ * @author R. Kempees
+ */
+public class NoCacheProvider implements PayloadCacheProvider, CacheInvalidator, CacheProvider {
+
+    private static final Logger LOG = LoggerFactory.getLogger(NoCacheProvider.class);
+
+    public NoCacheProvider () {
+        LOG.info("NoCacheProvider loaded. This means the DD4T Object Cache will not cache anything.");
+    }
+
+    @Override
+    public void flush () {
+        LOG.debug("Not flushing as this is nothing is cached.");
+    }
+
+    @Override
+    public void invalidate (final String key) {
+        LOG.debug("Nothing to invalidate by design.");
+    }
+
+    @Override
+    public Object loadFromLocalCache (final String key) {
+        LOG.debug("Nothing to load by design.");
+        return null;
+    }
+
+    @Override
+    public void storeInCache (final String key, final Cachable ob, final Collection<Cachable> deps) {
+        LOG.debug("Nothing to store by design.");
+    }
+
+    @Override
+    public void storeInItemCache (final String key, final Object ob, final int dependingPublicationId, final int dependingItemId) {
+        LOG.debug("Nothing to store by design.");
+    }
+
+    @Override
+    public void storeInComponentPresentationCache (final String key, final Object ob, final int dependingPublicationId, final int dependingCompId, final int dependingTemplateId) {
+        LOG.debug("Nothing to store by design.");
+    }
+
+    @Override
+    public void storeInKeywordCache (final String key, final Object ob, final int dependingPublicationId, final int dependingItemId) {
+        LOG.debug("Nothing to store by design.");
+    }
+
+    @Override
+    public <T> CacheElement<T> loadPayloadFromLocalCache (final String key) {
+        LOG.debug("Nothing to load by design.");
+        return null;
+    }
+
+    @Override
+    public <T> void storeInItemCache (final String key, final CacheElement<T> cacheElement) {
+        LOG.debug("Nothing to store by design.");
+    }
+
+    @Override
+    public <T> void storeInItemCache (final String key, final CacheElement<T> cacheElement, final int dependingPublicationId, final int dependingItemId) {
+        LOG.debug("Nothing to store by design.");
+    }
+}

643f22a7578a0c244ab07ce1f9ae8bcdf226b742
Mykola Bilenko <mbilenko@sdl.com>
2016-12-20 14:37:00 +0200

    KCWA-238 updated TcmUtils.class with more generic methods for different namespaces

--- dxa-framework/dxa-common-api/src/main/java/com/sdl/webapp/common/util/TcmUtils.java
+++ dxa-framework/dxa-common-api/src/main/java/com/sdl/webapp/common/util/TcmUtils.java
@@ -2,9 +2,13 @@ package com.sdl.webapp.common.util;
 
 import com.sdl.webapp.common.api.model.entity.SitemapItem;
 import lombok.extern.slf4j.Slf4j;
+import org.dd4t.core.util.TCMURI;
+import org.dd4t.core.util.TCMURI.Namespace;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static org.dd4t.core.util.TCMURI.Namespace.TCM;
 
 /**
  * Simple utility functions to process TCM-URIs.
@@ -18,11 +22,11 @@ public final class TcmUtils {
 
     private static final int PAGE_ITEM_TYPE = 64;
 
-    private static final String TCM_S_S = "tcm:%s-%s";
+    private static final String S_S_S = "%s:%s-%s";
 
-    private static final String TCM_S_S_S = "tcm:%s-%s-%s";
+    private static final String S_S_S_S = "%s:%s-%s-%s";
 
-    private static final Pattern PATTERN = Pattern.compile("tcm:(\\d+)-(\\d+)(-(\\d+))?");
+    private static final Pattern PATTERN = Pattern.compile("(\\w+):(\\d+)-(\\d+)(-(\\d+))?");
 
     private TcmUtils() {
     }
@@ -34,7 +38,18 @@ public final class TcmUtils {
      * @return TCM URI for publication
      */
     public static String buildPublicationTcmUri(int publicationId) {
-        return buildPublicationTcmUriInternal(String.valueOf(publicationId));
+        return buildPublicationCmUri(TCM, publicationId);
+    }
+
+    /**
+     * Build a publication CM URI looking like <code>namespace:0-ID-1</code>.
+     *
+     * @param namespace CM URI namespace
+     * @param publicationId publication ID
+     * @return TCM URI for publication
+     */
+    public static String buildPublicationCmUri(Namespace namespace, int publicationId) {
+        return buildPublicationCmUriInternal(namespace, String.valueOf(publicationId));
     }
 
     /**
@@ -44,11 +59,22 @@ public final class TcmUtils {
      * @return TCM URI for publication
      */
     public static String buildPublicationTcmUri(String publicationId) {
-        return buildPublicationTcmUriInternal(publicationId);
+        return buildPublicationCmUri(TCM, publicationId);
     }
 
-    private static String buildPublicationTcmUriInternal(String publicationId) {
-        return String.format(TCM_S_S_S, 0, publicationId, PUBLICATION_ITEM_TYPE);
+    /**
+     * Build a publication CM URI looking like <code>namespace:0-ID-1</code>.
+     *
+     * @param namespace CM URI namespace
+     * @param publicationId publication ID
+     * @return TCM URI for publication
+     */
+    public static String buildPublicationCmUri(Namespace namespace, String publicationId) {
+        return buildPublicationCmUriInternal(namespace, publicationId);
+    }
+
+    private static String buildPublicationCmUriInternal(Namespace namespace, String publicationId) {
+        return String.format(S_S_S_S, namespace.getValue(), 0, publicationId, PUBLICATION_ITEM_TYPE);
     }
 
     /**
@@ -59,7 +85,19 @@ public final class TcmUtils {
      * @return TCM URI for template
      */
     public static String buildTemplateTcmUri(String publicationId, String itemId) {
-        return String.format(TCM_S_S_S, publicationId, itemId, TEMPLATE_ITEM_TYPE);
+        return buildTemplateCmUri(TCM, publicationId, itemId);
+    }
+
+    /**
+     * Build a template CM URI looking like <code>namespace:PUB_ID-ITEM_ID-32</code>.
+     *
+     * @param namespace CM URI namespace
+     * @param publicationId publication ID
+     * @param itemId        item ID
+     * @return TCM URI for template
+     */
+    public static String buildTemplateCmUri(Namespace namespace, String publicationId, String itemId) {
+        return String.format(S_S_S_S, namespace.getValue(), publicationId, itemId, TEMPLATE_ITEM_TYPE);
     }
 
     /**
@@ -70,7 +108,19 @@ public final class TcmUtils {
      * @return TCM URI for page
      */
     public static String buildPageTcmUri(String publicationId, String itemId) {
-        return String.format(TCM_S_S_S, publicationId, itemId, PAGE_ITEM_TYPE);
+        return buildPageCmUri(TCM, publicationId, itemId);
+    }
+
+    /**
+     * Build a template CM URI looking like <code>namespace:PUB_ID-ITEM_ID-32</code>.
+     *
+     * @param namespace CM URI namespace
+     * @param publicationId publication ID
+     * @param itemId        item ID
+     * @return TCM URI for page
+     */
+    public static String buildPageCmUri(Namespace namespace, String publicationId, String itemId) {
+        return String.format(S_S_S_S, namespace.getValue(), publicationId, itemId, PAGE_ITEM_TYPE);
     }
 
     /**
@@ -81,7 +131,19 @@ public final class TcmUtils {
      * @return short TCM URI
      */
     public static String buildTcmUri(String publicationId, String itemId) {
-        return buildTcmUriInternal(publicationId, itemId);
+        return buildCmUri(TCM, publicationId, itemId);
+    }
+
+    /**
+     * Build a short CM URI looking like <code>namespace:PUB_ID-ITEM_ID</code>.
+     *
+     * @param namespace CM URI namespace
+     * @param publicationId publication ID
+     * @param itemId        item ID
+     * @return short CM URI
+     */
+    public static String buildCmUri(Namespace namespace, String publicationId, String itemId) {
+        return buildCmUriInternal(namespace, publicationId, itemId);
     }
 
     /**
@@ -92,11 +154,23 @@ public final class TcmUtils {
      * @return short TCM URI
      */
     public static String buildTcmUri(int publicationId, int itemId) {
-        return buildTcmUriInternal(String.valueOf(publicationId), String.valueOf(itemId));
+        return buildCmUri(TCM, publicationId, itemId);
     }
 
-    private static String buildTcmUriInternal(String publicationId, String itemId) {
-        return String.format(TCM_S_S, publicationId, itemId);
+    /**
+     * Build a short CM URI looking like <code>namespace:PUB_ID-ITEM_ID</code>.
+     *
+     * @param namespace CM URI namespace
+     * @param publicationId publication ID
+     * @param itemId        item ID
+     * @return short CM URI
+     */
+    public static String buildCmUri(Namespace namespace, int publicationId, int itemId) {
+        return buildCmUriInternal(namespace, String.valueOf(publicationId), String.valueOf(itemId));
+    }
+
+    private static String buildCmUriInternal(Namespace namespace, String publicationId, String itemId) {
+        return String.format(S_S_S, namespace.getValue(), publicationId, itemId);
     }
 
     /**
@@ -108,7 +182,20 @@ public final class TcmUtils {
      * @return a TCM URI
      */
     public static String buildTcmUri(String publicationId, String itemId, String itemType) {
-        return buildTcmUriInternal(publicationId, itemId, itemType);
+        return buildCmUri(TCM, publicationId, itemId, itemType);
+    }
+
+    /**
+     * Build a CM URI looking like <code>namespace:PUB_ID-ITEM_ID-ITEM_TYPE</code>.
+     *
+     * @param namespace CM URI namespace
+     * @param publicationId publication ID
+     * @param itemId        item ID
+     * @param itemType      item type
+     * @return a CM URI
+     */
+    public static String buildCmUri(Namespace namespace, String publicationId, String itemId, String itemType) {
+        return buildCmUriInternal(namespace, publicationId, itemId, itemType);
     }
 
     /**
@@ -120,40 +207,53 @@ public final class TcmUtils {
      * @return a TCM URI
      */
     public static String buildTcmUri(int publicationId, int itemId, int itemType) {
-        return buildTcmUriInternal(String.valueOf(publicationId), String.valueOf(itemId), String.valueOf(itemType));
-    }
-
-    public static String buildTcmUriInternal(String publicationId, String itemId, String itemType) {
-        return String.format(TCM_S_S_S, publicationId, itemId, itemType);
+        return buildCmUri(TCM, publicationId, itemId, itemType);
     }
 
     /**
-     * Extracts item ID from a valid TCM URI.
+     * Build a CM URI looking like <code>namespace:PUB_ID-ITEM_ID-ITEM_TYPE</code>.
      *
-     * @param tcmUri tcm uri to process
+     * @param namespace CM URI namespace
+     * @param publicationId publication ID
+     * @param itemId        item ID
+     * @param itemType      item type
+     * @return a CM URI
+     */
+    public static String buildCmUri(Namespace namespace, int publicationId, int itemId, int itemType) {
+        return buildCmUriInternal(namespace, String.valueOf(publicationId), String.valueOf(itemId), String.valueOf(itemType));
+    }
+
+    public static String buildCmUriInternal(Namespace namespace, String publicationId, String itemId, String itemType) {
+        return String.format(S_S_S_S, namespace.getValue(), publicationId, itemId, itemType);
+    }
+
+    /**
+     * Extracts item ID from a valid CM URI.
+     *
+     * @param cmUri cm uri to process
      * @return item ID or <code>-1</code> if URI is not valid or null
      */
-    public static int getItemId(String tcmUri) {
-        return extractGroupFromTcm(tcmUri, 2);
+    public static int getItemId(String cmUri) {
+        return extractGroupFromCm(cmUri, 3);
     }
 
     /**
-     * Extracts publication ID from a valid TCM URI.
+     * Extracts publication ID from a valid CM URI.
      *
-     * @param tcmUri tcm uri to process
+     * @param cmUri cm uri to process
      * @return publication ID or <code>-1</code> if URI is not valid or null
      */
-    public static int getPublicationId(String tcmUri) {
-        return extractGroupFromTcm(tcmUri, 1);
+    public static int getPublicationId(String cmUri) {
+        return extractGroupFromCm(cmUri, 2);
     }
 
-    private static int extractGroupFromTcm(String tcmUri, int group) {
+    private static int extractGroupFromCm(String cmUri, int group) {
         int failed = -1;
-        if (tcmUri == null) {
+        if (cmUri == null) {
             return failed;
         }
 
-        Matcher matcher = PATTERN.matcher(tcmUri);
+        Matcher matcher = PATTERN.matcher(cmUri);
         return matcher.matches() ? Integer.parseInt(matcher.group(group)) : failed;
     }
 
@@ -166,8 +266,21 @@ public final class TcmUtils {
      * @return localized TCM URI of an item
      */
     public static String localizeTcmUri(String tcmUri, String publicationTcmUri) {
-        int publicationId = getItemId(publicationTcmUri);
-        return localizeTcmUri(tcmUri, publicationId);
+        return localizeCmUri(TCM, tcmUri, publicationTcmUri);
+    }
+
+    /**
+     * Localizes given CM URI to current publication.
+     * <p>E.g. <code>namespace:1-2-3</code> with publication URI <code>namespace:0-8-1</code> will look like <code>namespace:8-2-3</code>.</p>
+     *
+     * @param namespace         CM URI namespace
+     * @param cmUri             cm uri of item to localize
+     * @param publicationCmUri  CM URI of publication
+     * @return localized CM URI of an item
+     */
+    public static String localizeCmUri(Namespace namespace, String cmUri, String publicationCmUri) {
+        int publicationId = getItemId(publicationCmUri);
+        return localizeCmUri(namespace, cmUri, publicationId);
     }
 
     /**
@@ -179,14 +292,27 @@ public final class TcmUtils {
      * @return localized TCM URI of an item
      */
     public static String localizeTcmUri(String tcmUri, int publicationId) {
-        Matcher matcher = PATTERN.matcher(tcmUri);
+        return localizeCmUri(TCM, tcmUri, publicationId);
+    }
+
+    /**
+     * Localizes given CM URI to current publication.
+     * <p>E.g. <code>namespace:1-2-3</code> with publication ID <code>8</code> will look like <code>namespace:8-2-3</code>.</p>
+     *
+     * @param namespace     CM URI namespace
+     * @param cmUri         cm uri of item to localize
+     * @param publicationId publication ID
+     * @return localized CM URI of an item
+     */
+    public static String localizeCmUri(Namespace namespace, String cmUri, int publicationId) {
+        Matcher matcher = PATTERN.matcher(cmUri);
         if (!matcher.matches()) {
-            log.warn("TCM URI {} is not valid", tcmUri);
-            throw new IllegalArgumentException("TCM URI is not valid: " + tcmUri);
+            log.warn("CM URI {} is not valid", cmUri);
+            throw new IllegalArgumentException("CM URI is not valid: " + cmUri);
         }
-        String itemType = matcher.group(4);
-        return itemType == null ? buildTcmUri(publicationId, getItemId(tcmUri)) :
-                buildTcmUri(publicationId, getItemId(tcmUri), Integer.parseInt(itemType));
+        String itemType = matcher.group(5);
+        return itemType == null ? buildCmUri(namespace, publicationId, getItemId(cmUri)) :
+                buildCmUri(namespace, publicationId, getItemId(cmUri), Integer.parseInt(itemType));
 
     }
 

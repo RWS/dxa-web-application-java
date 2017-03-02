5fe188ab55d7528d4b8e61b4e49ab21c76293237
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2017-03-02 15:57:07 +0100

    TSI-2270 added support for R2 for XO module (TSI-1265)

--- dxa-framework/dxa-data-model/src/main/java/com/sdl/dxa/api/datamodel/model/RegionModelData.java
+++ dxa-framework/dxa-data-model/src/main/java/com/sdl/dxa/api/datamodel/model/RegionModelData.java
@@ -1,12 +1,18 @@
 package com.sdl.dxa.api.datamodel.model;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
-import lombok.Value;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
+import java.util.Map;
 
-@Value
+@ToString
+@NoArgsConstructor
+@Getter
 @EqualsAndHashCode(callSuper = true)
 @JsonTypeName
 public class RegionModelData extends ViewModelData {
@@ -18,4 +24,21 @@ public class RegionModelData extends ViewModelData {
     private List<EntityModelData> entities;
 
     private List<RegionModelData> regions;
+
+    @Builder
+    public RegionModelData(String schemaId, String htmlClasses, Map<String, Object> xpmMetadata, ContentModelData metadata, Map<String, Object> extensionData, MvcModelData mvcData, String name, String includePageUrl, List<EntityModelData> entities, List<RegionModelData> regions) { // NOSONAR
+        super(schemaId, htmlClasses, xpmMetadata, metadata, extensionData, mvcData);
+        this.name = name;
+        this.includePageUrl = includePageUrl;
+        this.entities = entities;
+        this.regions = regions;
+    }
+
+    @Builder
+    public RegionModelData(String name, String includePageUrl, List<EntityModelData> entities, List<RegionModelData> regions) {
+        this.name = name;
+        this.includePageUrl = includePageUrl;
+        this.entities = entities;
+        this.regions = regions;
+    }
 }

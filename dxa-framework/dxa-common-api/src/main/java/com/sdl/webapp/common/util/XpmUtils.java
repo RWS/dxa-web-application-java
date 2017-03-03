b76e3c00c81be363f867703b9226df132b0432fc
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2017-03-03 10:23:41 +0100

    TSI-2324 changed immutable map to mutable (TSI-1265)

--- dxa-framework/dxa-common-api/src/main/java/com/sdl/webapp/common/util/XpmUtils.java
+++ dxa-framework/dxa-common-api/src/main/java/com/sdl/webapp/common/util/XpmUtils.java
@@ -1,11 +1,11 @@
 package com.sdl.webapp.common.util;
 
-import com.google.common.collect.ImmutableMap;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 import org.joda.time.DateTime;
 import org.joda.time.format.ISODateTimeFormat;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static com.sdl.webapp.common.api.model.region.RegionModelImpl.INCLUDED_FROM_PAGE_FILE_NAME_XPM_METADATA_KEY;
@@ -17,6 +17,7 @@ public final class XpmUtils {
     private XpmUtils() {
     }
 
+    @FunctionalInterface
     private interface XpmBuilder {
 
         Map<String, Object> buildXpm();
@@ -42,13 +43,13 @@ public final class XpmUtils {
 
         @Override
         public Map<String, Object> buildXpm() {
-            return ImmutableMap.<String, Object>builder()
-                    .put("ComponentID", componentId)
-                    .put("ComponentModified", printDate(componentModified))
-                    .put("ComponentTemplateID", componentTemplateID)
-                    .put("ComponentTemplateModified", printDate(componentTemplateModified))
-                    .put("IsRepositoryPublished", isRepositoryPublished)
-                    .build();
+            Map<String, Object> map = new HashMap<>();
+            map.put("ComponentID", componentId);
+            map.put("ComponentModified", printDate(componentModified));
+            map.put("ComponentTemplateID", componentTemplateID);
+            map.put("ComponentTemplateModified", printDate(componentTemplateModified));
+            map.put("IsRepositoryPublished", isRepositoryPublished);
+            return map;
         }
     }
 
@@ -68,13 +69,13 @@ public final class XpmUtils {
 
         @Override
         public Map<String, Object> buildXpm() {
-            return ImmutableMap.<String, Object>builder()
-                    .put("PageID", pageID)
-                    .put("PageModified", printDate(pageModified))
-                    .put("PageTemplateID", pageTemplateID)
-                    .put("PageTemplateModified", printDate(pageTemplateModified))
-                    .put("CmsUrl", cmsUrl)
-                    .build();
+            Map<String, Object> map = new HashMap<>();
+            map.put("PageID", pageID);
+            map.put("PageModified", printDate(pageModified));
+            map.put("PageTemplateID", pageTemplateID);
+            map.put("PageTemplateModified", printDate(pageTemplateModified));
+            map.put("CmsUrl", cmsUrl);
+            return map;
         }
     }
 
@@ -90,11 +91,11 @@ public final class XpmUtils {
 
         @Override
         public Map<String, Object> buildXpm() {
-            return ImmutableMap.<String, Object>builder()
-                    .put(INCLUDED_FROM_PAGE_ID_XPM_METADATA_KEY, includedFromPageID)
-                    .put(INCLUDED_FROM_PAGE_TITLE_XPM_METADATA_KEY, includedFromPageTitle)
-                    .put(INCLUDED_FROM_PAGE_FILE_NAME_XPM_METADATA_KEY, includedFromPageFileName)
-                    .build();
+            Map<String, Object> map = new HashMap<>();
+            map.put(INCLUDED_FROM_PAGE_ID_XPM_METADATA_KEY, includedFromPageID);
+            map.put(INCLUDED_FROM_PAGE_TITLE_XPM_METADATA_KEY, includedFromPageTitle);
+            map.put(INCLUDED_FROM_PAGE_FILE_NAME_XPM_METADATA_KEY, includedFromPageFileName);
+            return map;
         }
     }
 

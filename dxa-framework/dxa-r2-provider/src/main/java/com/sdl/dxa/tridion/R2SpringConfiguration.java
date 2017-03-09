3c041ce9d9f803911aaa3b43ff1d8ee142c93382
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2017-03-09 18:13:35 +0100

    Fixed Model Mapping for model service

--- dxa-framework/dxa-r2-provider/src/main/java/com/sdl/dxa/tridion/R2SpringConfiguration.java
+++ dxa-framework/dxa-r2-provider/src/main/java/com/sdl/dxa/tridion/R2SpringConfiguration.java
@@ -1,8 +1,11 @@
 package com.sdl.dxa.tridion;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sdl.dxa.R2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -11,15 +14,25 @@ import org.springframework.web.client.RestTemplate;
 @R2
 @Slf4j
 @Configuration
+@EnableCaching
 public class R2SpringConfiguration {
 
-    @Autowired
-    private MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter;
 
+    @Autowired
+    @Qualifier("dxaR2ObjectMapper")
+    private ObjectMapper objectMapper;
+    
     @Bean
     public RestTemplate modelServiceRestTemplate() {
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(0, mappingJackson2HttpMessageConverter);
+        restTemplate.getMessageConverters().add(0, dxaR2MappingJackson2HttpMessageConverter());
         return restTemplate;
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter dxaR2MappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        jsonConverter.setObjectMapper(objectMapper);
+        return jsonConverter;
     }
 }

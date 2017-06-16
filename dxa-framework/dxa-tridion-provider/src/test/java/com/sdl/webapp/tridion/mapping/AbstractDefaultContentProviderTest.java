cd37163eb8e2897dcec750a039eabf44c656624a
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2017-06-16 15:02:33 +0200

    TSI-2530 Test fix (TSI-110)

--- dxa-framework/dxa-tridion-provider/src/test/java/com/sdl/webapp/tridion/mapping/AbstractDefaultContentProviderTest.java
+++ dxa-framework/dxa-tridion-provider/src/test/java/com/sdl/webapp/tridion/mapping/AbstractDefaultContentProviderTest.java
@@ -1,5 +1,6 @@
 package com.sdl.webapp.tridion.mapping;
 
+import com.sdl.webapp.common.api.WebRequestContext;
 import com.sdl.webapp.common.api.content.ConditionalEntityEvaluator;
 import com.sdl.webapp.common.api.content.ContentProviderException;
 import com.sdl.webapp.common.api.localization.Localization;
@@ -28,6 +29,7 @@ public class AbstractDefaultContentProviderTest {
         List<ConditionalEntityEvaluator> evaluators = Collections.emptyList();
 
         AbstractDefaultContentProvider provider = mock(AbstractDefaultContentProvider.class);
+        ReflectionTestUtils.setField(provider, "webRequestContext", mock(WebRequestContext.class));
         ReflectionTestUtils.setField(provider, "entityEvaluators", evaluators);
         when(provider.getPageModel(anyString(), any(Localization.class))).thenCallRealMethod();
         when(provider._loadPage(anyString(), any(Localization.class))).thenReturn(pageModel);

44cf4d67ad4dc1d8491fd78fa5db600e6d067a62
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2017-06-23 13:20:33 +0200

    Key generator missing bean fix

--- dxa-framework/dxa-common-api/src/main/java/com/sdl/dxa/caching/LocalizationAwareKeyGenerator.java
+++ dxa-framework/dxa-common-api/src/main/java/com/sdl/dxa/caching/LocalizationAwareKeyGenerator.java
@@ -5,9 +5,11 @@ import com.sdl.webapp.common.api.WebRequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.cache.interceptor.SimpleKeyGenerator;
+import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
 
+@Component
 public class LocalizationAwareKeyGenerator implements KeyGenerator {
 
     @SuppressWarnings("SpringAutowiredFieldsWarningInspection")

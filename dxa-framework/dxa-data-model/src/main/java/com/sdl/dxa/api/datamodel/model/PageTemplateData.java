72a7239b9d55586f971619ea7429bd043eab990c
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2017-08-04 15:32:12 +0200

    TSI-2681 Scratch implementation of DD4T to R2 converter (TSI-2660)

--- dxa-framework/dxa-data-model/src/main/java/com/sdl/dxa/api/datamodel/model/PageTemplateData.java
+++ dxa-framework/dxa-data-model/src/main/java/com/sdl/dxa/api/datamodel/model/PageTemplateData.java
@@ -1,10 +1,14 @@
 package com.sdl.dxa.api.datamodel.model;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import lombok.Value;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 import org.joda.time.DateTime;
 
-@Value
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)
 @JsonTypeName
 public class PageTemplateData {
 

c186148a2aa82ff0b0ed086e883a3b1f8c357f14
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2018-01-09 18:00:39 +0100

    Handle empty profile property, no stream

--- dxa-framework/dxa-common-api/src/main/java/com/sdl/webapp/common/util/InitializationUtils.java
+++ dxa-framework/dxa-common-api/src/main/java/com/sdl/webapp/common/util/InitializationUtils.java
@@ -5,7 +5,6 @@ import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.input.BOMInputStream;
-import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.io.ClassPathResource;
@@ -205,9 +204,12 @@ public final class InitializationUtils {
     private static void addActiveProfiles(ConfigurableWebApplicationContext servletAppContext, String activeProfiles) {
         if (!isEmpty(activeProfiles)) {
             ConfigurableEnvironment environment = servletAppContext.getEnvironment();
-            Arrays.stream(activeProfiles.split(","))
-                    .filter(StringUtils::isNotEmpty)
-                    .forEach(profile -> environment.addActiveProfile(profile.trim()));
+            for (String profile : activeProfiles.split(",")) {
+                String trimmed = profile.trim();
+                if (!trimmed.isEmpty()) {
+                    environment.addActiveProfile(trimmed);
+                }
+            }
         }
     }
 

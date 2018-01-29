137e519db5c2d6eec107e7d0ec749f0823e9b51b
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2018-01-29 13:17:52 +0100

    TSI-3011 Typo fix (TSI-3010)

--- dxa-framework/dxa-data-model/src/main/java/com/sdl/dxa/api/datamodel/json/Polymorphic.java
+++ dxa-framework/dxa-data-model/src/main/java/com/sdl/dxa/api/datamodel/json/Polymorphic.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sdl.dxa.api.datamodel.DataModelSpringConfiguration;
 
 /**
- * Annotation that marks object as polymorphic meaning that it mey have subclasses.
+ * Annotation that marks object as polymorphic meaning that it may have subclasses.
  * Used by {@link ObjectMapper} to set {@link PolymorphicObjectMixin} in {@link DataModelSpringConfiguration}.
  *
  * @dxa.publicApi

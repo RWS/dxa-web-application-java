ef1a80e4777e4cd17be678f5c2c9f2cff1dac39f
Igor Nikonov <inikonov@sdl.com>
2018-07-18 15:21:05 +0300

    TSI-3138 removed unused import

--- dxa-framework/dxa-tridion-provider/src/main/java/com/sdl/webapp/tridion/TridionLocalizationResolver.java
+++ dxa-framework/dxa-tridion-provider/src/main/java/com/sdl/webapp/tridion/TridionLocalizationResolver.java
@@ -14,7 +14,6 @@ import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriUtils;
 

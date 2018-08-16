44aabbc0307c8222164df1e5102fcbdc0dc5eeca
Igor Nikonov <inikonov@sdl.com>
2018-08-16 17:24:25 +0300

    TSI-3414 fixed too large claims (ISH module uses only 1 single claim 'taf:ish:usercondition')

--- dxa-model-service-client/src/main/java/com/sdl/dxa/tridion/modelservice/ModelServiceClient.java
+++ dxa-model-service-client/src/main/java/com/sdl/dxa/tridion/modelservice/ModelServiceClient.java
@@ -7,7 +7,6 @@ import com.sdl.web.client.impl.OAuthTokenProvider;
 import com.tridion.ambientdata.AmbientDataContext;
 import com.tridion.ambientdata.claimstore.ClaimStore;
 import com.tridion.ambientdata.web.WebClaims;
-import com.tridion.ambientdata.web.WebContext;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -20,10 +19,8 @@ import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import javax.cache.annotation.CacheResult;
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.util.Arrays;
-import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -60,16 +57,14 @@ public class ModelServiceClient {
     private <T> T _makeRequest(String serviceUrl, Class<T> type, boolean isRetry, Object... params) throws ItemNotFoundInModelServiceException {
         try {
             HttpHeaders headers = new HttpHeaders();
-            processTafCookies(headers);
+            processModuleSpecificCookies(headers);
             processPreviewToken(headers);
             processAccessToken(headers, isRetry);
-            log.warn("Sending GET request to " + serviceUrl + " with parameters: " + Arrays.toString(params));
+            log.debug("Sending GET request to " + serviceUrl + " with parameters: " + Arrays.toString(params));
             ResponseEntity<T> response = restTemplate.exchange(serviceUrl, HttpMethod.GET, new HttpEntity<>(null, headers), type, params);
             return response.getBody();
         } catch (HttpStatusCodeException e) {
             HttpStatus statusCode = e.getStatusCode();
-            log.info("Got error response with a status code {} and body '{}' with message '{}' and response headers: {}", statusCode, e.getResponseBodyAsString(), e.getMessage(), e.getResponseHeaders() );
-
             if (statusCode.is4xxClientError()) {
                 if (statusCode == HttpStatus.NOT_FOUND) {
                     String message = "Item not found requesting '" + serviceUrl + "' with params '" + Arrays.toString(params) + "'";
@@ -79,6 +74,7 @@ public class ModelServiceClient {
                     log.warn("Got 401 status code, reason: {}, check if token is expired and retry if so ", statusCode.getReasonPhrase(), e);
                     return _makeRequest(serviceUrl, type, true, params);
                 } else {
+                    log.warn("Got error response with a status code {} and body '{}' with message '{}' and response headers: {}", statusCode, e.getResponseBodyAsString(), e.getMessage(), e.getResponseHeaders() );
                     String message = "Wrong request to the model service: " + serviceUrl + ", reason: " + statusCode.getReasonPhrase() + " error code: " + statusCode.value();
                     log.error(message, e);
                     throw new ModelServiceBadRequestException(message, e);
@@ -90,20 +86,11 @@ public class ModelServiceClient {
         }
     }
 
-    private void processTafCookies(HttpHeaders headers) {
-        ClaimStore claimStore = WebContext.getCurrentClaimStore();
-        if (claimStore == null) return;
-        for (Map.Entry<URI, Object> entry : claimStore.getAll().entrySet()) {
-            String key = entry.getKey().toString();
-            if (!key.startsWith("taf:")) continue;
-            try {
-                byte[] bytes = entry.getValue().toString().getBytes("UTF-8");
-                String value = Base64.getEncoder().encodeToString(bytes);
-                headers.add(HttpHeaders.COOKIE, String.format("%s=%s", key.replace(":", "."), value));
-            } catch (UnsupportedEncodingException e) {
-                throw new RuntimeException("UTF-8 encoder is not found. This should be impossible. Are you using JVM?", e);
-            }
-        }
+    /**
+     * This method is subject to extend the behaviour of ModelService Client.
+     * @param headers Http Headers to be extended in particular module
+     */
+    protected void processModuleSpecificCookies(HttpHeaders headers) {
     }
 
     private void processPreviewToken(HttpHeaders headers) {

c7336016e04ffd93d595f5d128ce57f10416dc01
Rajesh Kumar <rsaminathan@sdl.com>
2018-10-29 19:57:02 +0530

    updated the unit tests cases to include specific tests for pagelinks, binarylinks and component links

--- dxa-framework/dxa-tridion-common/src/test/java/com/sdl/dxa/tridion/linking/PCALinkResolverTest.java
+++ dxa-framework/dxa-tridion-common/src/test/java/com/sdl/dxa/tridion/linking/PCALinkResolverTest.java
@@ -3,62 +3,58 @@ package com.sdl.dxa.tridion.linking;
 import com.sdl.dxa.tridion.pcaclient.DefaultPCAClientProvider;
 import com.sdl.dxa.tridion.pcaclient.PCAClientConfigurationLoader;
 import com.sdl.dxa.tridion.pcaclient.PCAClientProvider;
+import com.sdl.web.pca.client.DefaultPublicContentApi;
+import com.sdl.web.pca.client.PublicContentApi;
 import com.sdl.web.pca.client.auth.Authentication;
 import com.sdl.web.pca.client.contentmodel.enums.ContentNamespace;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.util.Assert;
 
 import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PCALinkResolverTest {
 
     @Mock
-    private PCALinkResolver pcaLinkResolver;
-
-    @Mock
-    private PCAClientConfigurationLoader pcaClientConfigurationLoader;
-
-    @Mock
     private PCAClientProvider pcaClientProvider;
 
     @Mock
-    private Authentication authentication;
+    private PublicContentApi publicContentApi;
+
+    @InjectMocks
+    private PCALinkResolver linkResovler = new PCALinkResolver();
 
     @Before
     public void setup(){
-        when(pcaClientConfigurationLoader.getServiceUrl()).thenReturn("http://10.100.93.148:8081/cd/api");
-        when(pcaClientConfigurationLoader.getConfiguration()).thenReturn(new Properties());
-        pcaClientProvider = new DefaultPCAClientProvider(pcaClientConfigurationLoader, authentication);
+        when(pcaClientProvider.getClient()).thenReturn(publicContentApi);
+
+        when(pcaClientProvider.getClient().resolvePageLink(ContentNamespace.Sites,2,3,false)).thenReturn("/index.html");
+
+        when(pcaClientProvider.getClient().resolveComponentLink(ContentNamespace.Sites,2,3,null,null,false)).thenReturn("/resolved-component-2");
+
+        when(pcaClientProvider.getClient().resolveBinaryLink(ContentNamespace.Sites,2,3,null,false)).thenReturn("/media/baloon.png");
+
     }
 
     @Test
     public void resolveLink() {
-        Assert.hasText("http://localhost:8882/", pcaLinkResolver.resolveLink("http://localhost:8882/","5",true));
-    }
+/*        String pageLinkresult = linkResovler.resolveLink("tcm:2-3-64", "2", false);
+        assertEquals(pageLinkresult, "/index.html");*/
 
-    @Test
-    public void _componentResolver() {
-        String result = pcaClientProvider.getClient().resolveComponentLink(ContentNamespace.Sites, 5, 301, null, null, false);
-        assertEquals("http://dxatest.ams.dev:8098/about/office-location.html", result);
-    }
+        String componentLinkresult = linkResovler.resolveLink("tcm:2-3", "2", false);
+        assertEquals(componentLinkresult, "/resolved-component-2");
 
-    @Test
-    public void _pageResolver() {
-       String result = pcaClientProvider.getClient().resolvePageLink(ContentNamespace.Sites, 5, 320, false);
-       assertEquals("http://dxatest.ams.dev:8098/articles/news/news3.html", result);
-    }
-
-    @Test
-    public void _binaryResolver() {
-        String result = pcaClientProvider.getClient().resolveBinaryLink(ContentNamespace.Sites, 5, 289, null, false);
-        assertEquals("http://dxatest.ams.dev:8098/media/wine-cellar_tcm5-289.jpg", result);
+        String binaryLinkresult = linkResovler.resolveLink("tcm:2-3", "2", true);
+        assertEquals(binaryLinkresult, "/media/baloon.png");
     }
 }

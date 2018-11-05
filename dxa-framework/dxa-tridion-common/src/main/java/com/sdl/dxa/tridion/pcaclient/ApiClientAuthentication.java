11f98c5c19e27376bf358d1f1c5b546fd75d0367
Mykola Bilenko <mbilenko@sdl.com>
2018-11-05 16:37:42 +0200

    TSI-3748 added profile annotation to api client provider and ApiClientAuthentication

--- dxa-framework/dxa-tridion-common/src/main/java/com/sdl/dxa/tridion/pcaclient/ApiClientAuthentication.java
+++ dxa-framework/dxa-tridion-common/src/main/java/com/sdl/dxa/tridion/pcaclient/ApiClientAuthentication.java
@@ -8,11 +8,13 @@ import org.apache.http.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import static com.sdl.dxa.tridion.common.ConfigurationConstants.AUTHORIZATION_HEADER;
 
 @Service("ApiClientAuthentication")
+@Profile("!cil.providers.active")
 public class ApiClientAuthentication implements Authentication {
     private static final Logger LOG = LoggerFactory.getLogger(ApiClientAuthentication.class);
 

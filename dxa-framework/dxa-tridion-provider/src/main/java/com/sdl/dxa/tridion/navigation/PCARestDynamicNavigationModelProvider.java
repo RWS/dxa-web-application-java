2bd1eb81474919a7b6b25538d9301f6eecb6df0b
Oleksii Kushnir <okushnir@sdl.com>
2018-11-06 17:05:03 +0200

    TSI-3494: Migrated code from .net implementation.

--- dxa-framework/dxa-tridion-provider/src/main/java/com/sdl/dxa/tridion/navigation/PCARestDynamicNavigationModelProvider.java
+++ dxa-framework/dxa-tridion-provider/src/main/java/com/sdl/dxa/tridion/navigation/PCARestDynamicNavigationModelProvider.java
@@ -10,7 +10,6 @@ import com.sdl.dxa.tridion.navigation.dynamic.NavigationModelProvider;
 import com.sdl.dxa.tridion.navigation.dynamic.OnDemandNavigationModelProvider;
 import com.sdl.dxa.tridion.pcaclient.PCAClientProvider;
 import com.sdl.web.pca.client.contentmodel.ContextData;
-import com.sdl.web.pca.client.contentmodel.enums.ContentNamespace;
 import com.sdl.web.pca.client.contentmodel.generated.Ancestor;
 import com.sdl.web.pca.client.contentmodel.generated.ClaimValue;
 import com.sdl.web.pca.client.contentmodel.generated.ClaimValueType;
@@ -29,14 +28,18 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static com.sdl.web.pca.client.contentmodel.enums.ContentNamespace.Sites;
+import static com.sdl.web.pca.client.contentmodel.generated.Ancestor.NONE;
 import static java.lang.String.format;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 
 @Slf4j
 @Service
@@ -70,6 +73,36 @@ public class PCARestDynamicNavigationModelProvider implements NavigationModelPro
         }
     }
 
+    private Optional<TaxonomySitemapItem> getNavigationModelInternal(@NotNull SitemapRequestDto request) {
+        int depth = getDepth(request);
+        ContextData contextData = createContextData(request.getClaims());
+
+        TaxonomySitemapItem taxonomySitemapItem = provider.getClient()
+                .getSitemap(Sites, request.getLocalizationId(), depth, contextData);
+        if (taxonomySitemapItem == null) {
+            return Optional.empty();
+        }
+
+        List<TaxonomySitemapItem> leafNodes = getLeafNodes(taxonomySitemapItem);
+        while (leafNodes.size() > 0) {
+            TaxonomySitemapItem node = leafNodes.remove(0);
+            if (node.getHasChildNodes()) {
+                TaxonomySitemapItem[] subtree = provider.getClient()
+                        .getSitemapSubtree(Sites, request.getLocalizationId(), node.getId(), depth, NONE, contextData);
+                if (node.getItems() == null) {
+                    node.setItems(new ArrayList<>());
+                }
+
+                if (subtree.length > 0 && subtree[0].getItems() != null) {
+                    node.getItems().addAll(subtree[0].getItems());
+                }
+                leafNodes.addAll(getLeafNodes(node));
+            }
+        }
+
+        return Optional.of(taxonomySitemapItem);
+    }
+
     @Override
     public Optional<Collection<SitemapItemModelData>> getNavigationSubtree(@NotNull SitemapRequestDto requestDto) {
         try {
@@ -77,7 +110,7 @@ public class PCARestDynamicNavigationModelProvider implements NavigationModelPro
             Collection<SitemapItemModelData> result = convert(navigation);
 
             // this needed because this interface method has a wrong result type
-            // usually, empty collections means 'NO DATA',
+            // usually, empty collections means 'NO DATA'
             if (result.isEmpty()) {
                 return Optional.empty();
             } else {
@@ -91,64 +124,81 @@ public class PCARestDynamicNavigationModelProvider implements NavigationModelPro
 
     private List<SitemapItem> getNavigationSubtreeInternal(@NotNull SitemapRequestDto requestDto) {
         int depth = getDepth(requestDto);
+        ContextData contextData = createContextData(requestDto.getClaims());
 
-        TaxonomySitemapItem[] taxonomySitemapItem = provider.getClient().getSitemapSubtree(ContentNamespace.Sites,
-                requestDto.getLocalizationId(),
-                requestDto.getSitemapId(),
-                depth,
-                requestDto.getNavigationFilter().isWithAncestors() ? Ancestor.INCLUDE : Ancestor.NONE,
-                createContextData(requestDto.getClaims()));
-
-
-        List<SitemapItemModelData> result = new ArrayList<>();
-
-
-        if (taxonomySitemapItem != null) {
-            for (TaxonomySitemapItem item : taxonomySitemapItem) {
-                result.add(convert(item));
+        if (requestDto.getExpandLevels().isUnlimited()) {
+            List<SitemapItem> entireTree = getEntireNavigationSubtreeInternal(requestDto);
+            if (requestDto.getSitemapId() == null) {
+                // if parent node not specified we return entire tree
+                return entireTree;
+            } else {
+                // root node specified so return the direct children of this node
+                return entireTree.stream()
+                        .flatMap(sitemapItem -> ((TaxonomySitemapItem) sitemapItem).getItems().stream())
+                        .sorted(Comparator.comparing(sitemapItem -> sitemapItem.getOriginalTitle()))
+                        .collect(Collectors.toList());
+            }
+        } else {
+            String sitemapId = requestDto.getSitemapId();
+            boolean withAncestors = requestDto.getNavigationFilter().isWithAncestors();
+            if (sitemapId == null) {
+                // Requesting from root so just return descendants from root
+                int adjustedDepth = depth > 0 ? depth - 1 : depth;
+                return asList(provider.getClient().getSitemapSubtree(Sites, requestDto.getLocalizationId(),
+                        null, adjustedDepth, withAncestors ? Ancestor.INCLUDE : NONE, contextData));
+            } else if (withAncestors) {
+                // we are looking for a particular item, we need to request the entire subtree first
+                List<SitemapItem> entireTree = getEntireNavigationSubtreeInternal(requestDto);
+                // Prune descendants from our deseried node
+                SitemapItem node = findNode(entireTree, sitemapId);
+                prune(node, 0, depth);
+                return entireTree;
+            } else {
+                return asList(provider.getClient()
+                        .getSitemapSubtree(Sites, requestDto.getLocalizationId(), sitemapId, depth, NONE, contextData))
+                        .stream().map(item -> item.getItems())
+                        .filter(sitemapItems -> sitemapItems != null)
+                        .flatMap(sitemapItems -> sitemapItems.stream())
+                        .sorted(Comparator.comparing(sitemapItem -> sitemapItem.getOriginalTitle()))
+                        .collect(Collectors.toList());
             }
         }
-        return Optional.of(result);
     }
 
-    private Optional<TaxonomySitemapItem> getNavigationModelInternal(@NotNull SitemapRequestDto requestDto) {
-        int depth = getDepth(requestDto);
+    private List<SitemapItem> getEntireNavigationSubtreeInternal(@NotNull SitemapRequestDto request) {
+        int depth = getDepth(request);
+        ContextData contextData = createContextData(request.getClaims());
 
-        TaxonomySitemapItem taxonomySitemapItem = provider.getClient().getSitemap(
-                ContentNamespace.Sites,
-                requestDto.getLocalizationId(),
-                depth,
-                createContextData(requestDto.getClaims()));
-        if (taxonomySitemapItem == null) {
-            return Optional.empty();
-        }
-
-        List<TaxonomySitemapItem> leafNodes = getLeafNodes(taxonomySitemapItem);
-        while (leafNodes.size() > 0) {
-            TaxonomySitemapItem node = leafNodes.remove(0);
-            if (node.getHasChildNodes()) {
-                TaxonomySitemapItem[] subtree = provider.getClient().getSitemapSubtree(
-                        ContentNamespace.Sites,
-                        requestDto.getLocalizationId(),
-                        node.getId(),
+        List<SitemapItem> rootItems = asList(provider.getClient()
+                .getSitemapSubtree(Sites,
+                        request.getLocalizationId(),
+                        request.getSitemapId(),
                         depth,
-                        Ancestor.NONE,
-                        createContextData(requestDto.getClaims()));
-                if (node.getItems() == null) {
-                    node.setItems(new ArrayList<>());
-                }
+                        request.getNavigationFilter().isWithAncestors() ? Ancestor.INCLUDE : NONE,
+                        contextData));
 
-                if (subtree != null && subtree.length > 0 && subtree[0].getItems() != null) {
-                    node.getItems().addAll(subtree[0].getItems());
-                }
-                leafNodes.addAll(getLeafNodes(node));
-            }
+        if (rootItems.isEmpty()) {
+            return emptyList();
         }
 
-        return Optional.of(taxonomySitemapItem);
+        List<SitemapItem> tempRoots = new ArrayList<>(rootItems);
+
+        int index = 0;
+        while (index < tempRoots.size()) {
+            TaxonomySitemapItem root = (TaxonomySitemapItem) tempRoots.get(index);
+            List<TaxonomySitemapItem> leafNodes = getLeafNodes(root);
+            for (TaxonomySitemapItem item : leafNodes) {
+                TaxonomySitemapItem[] subtree = provider.getClient()
+                        .getSitemapSubtree(Sites, request.getLocalizationId(), item.getId(), depth, NONE, contextData);
+                if (subtree.length > 0) {
+                    item.setItems(subtree[0].getItems());
+                    tempRoots.addAll(getLeafNodes(item));
+                }
+            }
+            index++;
+        }
+        return rootItems;
     }
-
-
 
     private List<TaxonomySitemapItem> getLeafNodes(SitemapItem rootNode) {
         List<TaxonomySitemapItem> leafNodes = new ArrayList<>();
@@ -162,7 +212,7 @@ public class PCARestDynamicNavigationModelProvider implements NavigationModelPro
         }
 
         if (root.getItems() == null || root.getItems().isEmpty()) {
-            return new ArrayList<>(Arrays.asList(root));
+            return new ArrayList<>(asList(root));
         }
 
         for (SitemapItem item : root.getItems()) {
@@ -179,6 +229,47 @@ public class PCARestDynamicNavigationModelProvider implements NavigationModelPro
         }
 
         return leafNodes;
+    }
+
+    private SitemapItem findNode(List<SitemapItem> roots, String sitemapId) {
+        for (SitemapItem node : roots) {
+            if (node.getId().equals(sitemapId)) {
+                return node;
+            }
+
+            if (node instanceof TaxonomySitemapItem) {
+                TaxonomySitemapItem item = (TaxonomySitemapItem) node;
+                if (item.getItems() != null && item.getItems().size() > 0) {
+                    SitemapItem result = findNode(item.getItems(), sitemapId);
+                    if (result != null) {
+                        return result;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private void prune(SitemapItem root, int currentLevel, int descendantLevels) {
+        if (!(root instanceof TaxonomySitemapItem)) {
+            return;
+        }
+
+        TaxonomySitemapItem item = (TaxonomySitemapItem) root;
+        if (item.getItems() == null || item.getItems().size() == 0) {
+            return;
+        }
+
+        if (currentLevel < descendantLevels) {
+            for (SitemapItem next : item.getItems()) {
+                prune(next, currentLevel + 1, descendantLevels);
+            }
+        } else {
+            if (item.getItems() != null) {
+                item.getItems().clear();
+            }
+        }
     }
 
     private int getDepth(@NotNull SitemapRequestDto requestDto) {

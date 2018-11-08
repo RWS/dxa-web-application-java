93b73401080faacf3cb700e1ce44befb35888a0a
Igor Nikonov <inikonov@sdl.com>
2018-11-08 14:49:20 +0200

    TSI-3769 refactored code to make it simplier for testing

--- dxa-framework/dxa-tridion-provider/src/main/java/com/sdl/dxa/tridion/mapping/impl/DefaultBinaryProvider.java
+++ dxa-framework/dxa-tridion-provider/src/main/java/com/sdl/dxa/tridion/mapping/impl/DefaultBinaryProvider.java
@@ -5,14 +5,12 @@ import com.sdl.webapp.common.api.content.ContentProviderException;
 import com.sdl.webapp.common.api.content.StaticContentItem;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.regex.Pattern;
 
 @Slf4j
 public class DefaultBinaryProvider {
@@ -24,26 +22,30 @@ public class DefaultBinaryProvider {
         this.webApplicationContext = webApplicationContext;
     }
 
-    public StaticContentItem getStaticContent(ContentProvider provider, int binaryId, String localizationId, String localizationPath) throws ContentProviderException {
+    public StaticContentItem getStaticContent(ContentProvider provider, int binaryId, String localizationId, String localizationPath)
+            throws ContentProviderException {
+        Path pathToBinaries = getPathToBinaryFiles(localizationId);
+        String[] files = pathToBinaries.toFile().list((path, name) -> name.matches(".*_tcm" + localizationId + "-" + binaryId + "\\D.*"));
+        return processBinaryFile(provider, localizationId, localizationPath, files);
+    }
+
+    @NotNull
+    private Path getPathToBinaryFiles(String localizationId) {
+        String parentPath = StringUtils.join(new String[]{getBasePath(), STATIC_FILES_DIR, localizationId, "media"}, File.separator);
+        return Paths.get(parentPath);
+    }
+
+    private StaticContentItem processBinaryFile(ContentProvider provider, String localizationId, String localizationPath, String[] files) throws ContentProviderException {
+        if (files == null || files.length <= 0) {
+            return null;
+        }
+        return provider.getStaticContent(files[0], localizationId, localizationPath));
+    }
+
+    @NotNull
+    private String getBasePath() {
         String basePath = webApplicationContext.getServletContext().getRealPath("/");
         if (basePath.endsWith(File.separator)) basePath = basePath.substring(0, basePath.length()-1);
-        String parentPath = StringUtils.join(new String[]{basePath, STATIC_FILES_DIR, localizationId, "media"}, File.separator);
-        Path pathToBinaries = Paths.get(parentPath.trim());
-        AtomicReference<StaticContentItem> result = new AtomicReference<>();
-        AtomicReference<ContentProviderException> exception = new AtomicReference<>();
-        String[] files = pathToBinaries.toFile().list((path, name) -> name.matches(".*_tcm" + localizationId + "-" + binaryId + "\\D.*"));
-        if (files != null && files.length > 0) {
-            String path = files[0];
-            try {
-                result.set(provider.getStaticContent(path, localizationId, localizationPath));
-            } catch (ContentProviderException e) {
-                exception.set(new ContentProviderException("Binary content cannot be loaded by path " + path, e));
-            }
-        }
-        if (exception.get() != null) throw exception.get();
-        if (result.get() == null) {
-            log.info("Binary content for publication " + localizationId + " and id " + binaryId + " is not found");
-        }
-        return result.get();
+        return basePath;
     }
 }

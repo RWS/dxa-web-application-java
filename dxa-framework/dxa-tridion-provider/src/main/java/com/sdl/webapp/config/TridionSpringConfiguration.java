c143c521a1bff8259bef2daf9877557198ea6945
Sergii Kompanets <skompanets@sdl.com>
2018-11-08 10:30:02 +0200

    TSI-3693: added profile "cil.providers.active" for beans in config DynamicMetaRetriever, BinaryContentRetriever, DynamicMappingsRetriever

--- dxa-framework/dxa-tridion-provider/src/main/java/com/sdl/webapp/config/TridionSpringConfiguration.java
+++ dxa-framework/dxa-tridion-provider/src/main/java/com/sdl/webapp/config/TridionSpringConfiguration.java
@@ -8,9 +8,11 @@ import com.tridion.dynamiccontent.DynamicMetaRetriever;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @ComponentScan({"com.sdl.webapp.tridion", "com.sdl.dxa.tridion"})
 @Configuration
+@Profile("cil.providers.active")
 public class TridionSpringConfiguration {
 
     @Bean

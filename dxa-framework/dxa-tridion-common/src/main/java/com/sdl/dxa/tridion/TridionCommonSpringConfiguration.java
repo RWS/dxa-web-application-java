c61ac8e38add3a50849dff48ed0ea7ec82161823
Oleksii Kushnir <okushnir@sdl.com>
2018-11-13 15:33:11 +0200

    TSI-3693: Move beans to proper configuration class and added cil.providers.active profile.

--- dxa-framework/dxa-tridion-common/src/main/java/com/sdl/dxa/tridion/TridionCommonSpringConfiguration.java
+++ dxa-framework/dxa-tridion-common/src/main/java/com/sdl/dxa/tridion/TridionCommonSpringConfiguration.java
@@ -1,23 +1,50 @@
 package com.sdl.dxa.tridion;
 
+import com.sdl.web.api.content.BinaryContentRetriever;
+import com.sdl.web.api.dynamic.BinaryContentRetrieverImpl;
 import com.sdl.web.api.dynamic.taxonomies.WebTaxonomyFactory;
+import com.sdl.web.api.meta.WebPublicationMetaFactory;
+import com.sdl.web.api.meta.WebPublicationMetaFactoryImpl;
 import com.sdl.web.api.taxonomies.WebTaxonomyFactoryImpl;
+import com.tridion.dynamiccontent.DynamicMetaRetriever;
 import com.tridion.taxonomies.TaxonomyRelationManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @ComponentScan("com.sdl.dxa.tridion")
 @Configuration
 public class TridionCommonSpringConfiguration {
 
     @Bean
+    @Profile("cil.providers.active")
     public WebTaxonomyFactory webTaxonomyFactory() {
         return new WebTaxonomyFactoryImpl();
     }
 
     @Bean
+    @Profile("cil.providers.active")
     public TaxonomyRelationManager taxonomyRelationManager() {
         return new TaxonomyRelationManager();
     }
+
+    @Bean
+    @Profile("cil.providers.active")
+    public DynamicMetaRetriever dynamicMetaRetriever() {
+        return new DynamicMetaRetriever();
+    }
+
+    @Bean
+    @Profile("cil.providers.active")
+    public BinaryContentRetriever binaryContentRetriever() {
+        return new BinaryContentRetrieverImpl();
+    }
+
+    @Bean
+    @Profile("cil.providers.active")
+    public WebPublicationMetaFactory webPublicationMetaFactory() {
+        return new WebPublicationMetaFactoryImpl();
+    }
+
 }
--- dxa-framework/dxa-tridion-provider/src/main/java/com/sdl/webapp/config/TridionSpringConfiguration.java
+++ dxa-framework/dxa-tridion-provider/src/main/java/com/sdl/webapp/config/TridionSpringConfiguration.java
@@ -1,10 +1,7 @@
 package com.sdl.webapp.config;
 
-import com.sdl.web.api.content.BinaryContentRetriever;
-import com.sdl.web.api.dynamic.BinaryContentRetrieverImpl;
 import com.sdl.web.api.dynamic.DynamicMappingsRetriever;
 import com.sdl.web.api.dynamic.DynamicMappingsRetrieverImpl;
-import com.tridion.dynamiccontent.DynamicMetaRetriever;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,18 +10,6 @@ import org.springframework.context.annotation.Profile;
 @ComponentScan({"com.sdl.webapp.tridion", "com.sdl.dxa.tridion"})
 @Configuration
 public class TridionSpringConfiguration {
-
-    @Bean
-    @Profile("cil.providers.active")
-    public DynamicMetaRetriever dynamicMetaRetriever() {
-        return new DynamicMetaRetriever();
-    }
-
-    @Bean
-    @Profile("cil.providers.active")
-    public BinaryContentRetriever binaryContentRetriever() {
-        return new BinaryContentRetrieverImpl();
-    }
 
     @Bean
     @Profile("cil.providers.active")

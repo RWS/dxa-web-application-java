3db2fdaaa3a0df9f3196f5f6606d66b6c3c8819f
Anton Lytvyn <alytvyn@sdl.com>
2018-11-20 12:16:31 +0200

    TSI-3793: adding OAuth header when retrieving binaries

--- dxa-framework/dxa-tridion-common/src/main/java/com/sdl/dxa/tridion/content/BinaryContentDownloader.java
+++ dxa-framework/dxa-tridion-common/src/main/java/com/sdl/dxa/tridion/content/BinaryContentDownloader.java
@@ -1,11 +1,13 @@
 package com.sdl.dxa.tridion.content;
 
+import com.sdl.web.pca.client.auth.Authentication;
 import com.sdl.webapp.common.api.content.StaticContentNotLoadedException;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -16,13 +18,17 @@ public class BinaryContentDownloader {
 
     private final CloseableHttpClient httpclient;
 
+    @Autowired
+    private Authentication authentication;
+
     public BinaryContentDownloader() {
         httpclient = HttpClients.createDefault();
     }
 
     public byte[] downloadContent(File file, String downloadUrl) throws StaticContentNotLoadedException {
         HttpGet httpget = new HttpGet(downloadUrl);
-        try(CloseableHttpResponse response = httpclient.execute(httpget)) {
+        authentication.applyManualAuthentication(httpget);
+        try (CloseableHttpResponse response = httpclient.execute(httpget)) {
             return IOUtils.toByteArray(response.getEntity().getContent());
         } catch (IOException e) {
             throw new StaticContentNotLoadedException("Cannot content for file " + file, e);

63ca32454499c31084b78e684c6eaad138d60c9b
mborysenko <mborysenko@sdl.com>
2019-05-14 16:24:54 +0300

    TSI-3949 > Improved unit test

--- dxa-framework/dxa-common-api/src/test/java/com/sdl/webapp/common/impl/localization/LocalizationFactoryImplTest.java
+++ dxa-framework/dxa-common-api/src/test/java/com/sdl/webapp/common/impl/localization/LocalizationFactoryImplTest.java
@@ -4,51 +4,54 @@ import com.sdl.webapp.common.api.localization.LocalizationFactoryException;
 import com.sdl.webapp.common.api.mapping.semantic.config.FieldSemantics;
 import com.sdl.webapp.common.api.mapping.semantic.config.SemanticField;
 import com.sdl.webapp.common.api.mapping.semantic.config.SemanticSchema;
-import com.sdl.webapp.common.api.mapping.semantic.config.SemanticVocabulary;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static com.sdl.webapp.common.api.mapping.semantic.config.SemanticVocabulary.SDL_CORE_VOCABULARY;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class LocalizationFactoryImplTest {
+
+    private LocalizationFactoryImpl localizationFactory;
+
+    @Before
+    public void init() {
+        this.localizationFactory = new LocalizationFactoryImpl();
+    }
 
 
     @Test
     public void shouldAddDocsTopicSchema() throws LocalizationFactoryException {
-        LocalizationFactoryImpl factory = new LocalizationFactoryImpl();
 
-        SemanticSchema topicSchema = factory.getTopicSchema();
+        SemanticSchema topicSchema = localizationFactory.getTopicSchema();
         assertEquals(1, topicSchema.getId());
 
         Map<FieldSemantics, SemanticField> semanticFields = topicSchema.getSemanticFields();
         assertEquals(2, semanticFields.size());
 
-        String topicTitleElem = "topicTitle";
-        String topicBodyElem = "topicBody";
-
-        Optional<Map.Entry<FieldSemantics, SemanticField>> titleSemantics = semanticFields.entrySet()
-                .stream()
-                .filter(entry -> entry.getKey().getPropertyName().equals(topicTitleElem))
-                .findFirst();
-
-        assertTrue(titleSemantics.isPresent());
-        assertEquals(topicTitleElem, titleSemantics.get().getKey().getPropertyName());
-        assertEquals(SDL_CORE_VOCABULARY, titleSemantics.get().getKey().getVocabulary());
-        assertEquals("title", titleSemantics.get().getValue().getName());
-
-        Optional<Map.Entry<FieldSemantics, SemanticField>> bodySemantics = semanticFields.entrySet()
-                .stream()
-                .filter(entry -> entry.getKey().getPropertyName().equals(topicBodyElem))
-                .findFirst();
-
-        assertTrue(bodySemantics.isPresent());
-        assertEquals(topicBodyElem, bodySemantics.get().getKey().getPropertyName());
-        assertEquals(SDL_CORE_VOCABULARY, bodySemantics.get().getKey().getVocabulary());
-        assertEquals("topic", bodySemantics.get().getValue().getName());
+        ensureOnlyOneField(semanticFields, "topicTitle", "title");
+        ensureOnlyOneField(semanticFields, "topicBody", "topic");
     }
 
+    private List<Map.Entry<FieldSemantics, SemanticField>> getSemanticsForElement(Map<FieldSemantics, SemanticField> fields, String elementName) {
+        return fields.entrySet()
+                .stream()
+                .filter(entry -> entry.getKey().getPropertyName().equals(elementName))
+                .collect(Collectors.toList());
+    }
+
+    private void ensureOnlyOneField(Map<FieldSemantics, SemanticField> fields, String elementName, String fieldName) {
+        List<Map.Entry<FieldSemantics, SemanticField>> semanticFields = getSemanticsForElement(fields, elementName);
+        assertEquals(1, semanticFields.size());
+
+        Map.Entry<FieldSemantics, SemanticField> fieldSemantics = semanticFields.get(0);
+        assertEquals(elementName, fieldSemantics.getKey().getPropertyName());
+        assertEquals(SDL_CORE_VOCABULARY, fieldSemantics.getKey().getVocabulary());
+        assertEquals(fieldName, fieldSemantics.getValue().getName());
+
+    }
 }

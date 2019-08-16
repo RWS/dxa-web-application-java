e652a1d294153920caa5ea89c080dc2669022d29
Maksym Borysenko <mborysenko@sdl.com>
2019-08-16 09:34:19 +0200

    CRQ-15993 > Removed unused import

--- dxa-model-service-client/src/main/java/com/sdl/dxa/tridion/modelservice/ModelServiceClientConfiguration.java
+++ dxa-model-service-client/src/main/java/com/sdl/dxa/tridion/modelservice/ModelServiceClientConfiguration.java
@@ -14,7 +14,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
-import javax.ws.rs.Produces;
 import java.util.Objects;
 import java.util.Optional;
 

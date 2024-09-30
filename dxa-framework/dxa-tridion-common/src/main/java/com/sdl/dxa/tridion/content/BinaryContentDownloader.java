0a35e15c5255c71760caf24bf57b8ee8759e2454
dxasupport <ps.dxa.support@sdl.com>
2024-09-30 11:33:16 +0100

    Update to mitigate rare condition when downloading binary files from inside pods

--- dxa-framework/dxa-tridion-common/src/main/java/com/sdl/dxa/tridion/content/BinaryContentDownloader.java
+++ dxa-framework/dxa-tridion-common/src/main/java/com/sdl/dxa/tridion/content/BinaryContentDownloader.java
@@ -1,9 +1,11 @@
 package com.sdl.dxa.tridion.content;
 
 import com.sdl.web.pca.client.auth.Authentication;
+import com.sdl.web.pca.client.exception.UnauthorizedException;
 import com.sdl.webapp.common.api.content.StaticContentNotLoadedException;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -15,10 +17,13 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Component
 @Profile("!cil.providers.active")
 public class BinaryContentDownloader {
+
+    private static final Integer TIMEOUT_IN_MILLIS = 10000;
 
     private final CloseableHttpClient httpclient;
 
@@ -46,11 +51,48 @@ public class BinaryContentDownloader {
     }
 
     public byte[] downloadContent(File file, String downloadUrl) throws StaticContentNotLoadedException {
-        HttpGet httpget = new HttpGet(downloadUrl);
-        authentication.applyManualAuthentication(httpget);
-        try (CloseableHttpResponse response = httpclient.execute(httpget)) {
-            return IOUtils.toByteArray(response.getEntity().getContent());
-        } catch (IOException e) {
+        int attempt = 3;
+        UnauthorizedException[] exception = new UnauthorizedException[1];
+        while(true) {
+            if (attempt > 0) {
+                try {
+                    --attempt;
+                    return downloadContentInternal(file, downloadUrl);
+                }
+                catch (UnauthorizedException unauthorizedException) {
+                    if (exception[0] == null) {
+                        exception[0] = unauthorizedException;
+                    }
+                    try {
+                        Thread.currentThread();
+                        Thread.sleep(200L);
+                        continue;
+                    }
+                    catch (InterruptedException var6) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+            throw new StaticContentNotLoadedException("Cannot download content for file " + file, exception[0]);
+        }
+    }
+
+    public byte[] downloadContentInternal(File file, String downloadUrl) throws UnauthorizedException, StaticContentNotLoadedException {
+        HttpGet httpGet = new HttpGet(downloadUrl);
+        RequestConfig params = RequestConfig.custom().setConnectTimeout(TIMEOUT_IN_MILLIS).setSocketTimeout(TIMEOUT_IN_MILLIS).build();
+        httpGet.setConfig(params);
+        authentication.applyManualAuthentication(httpGet);
+        try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
+            InputStream contentStream = response.getEntity().getContent();
+            if (response.getStatusLine().getStatusCode() != 200) {
+                if (response.getStatusLine().getStatusCode() == 401) {
+                    throw new UnauthorizedException("Unable to retrieve requested entity");
+                }
+                throw new StaticContentNotLoadedException("Unable to retrieve requested entity from " + downloadUrl);
+            }
+            return IOUtils.toByteArray(contentStream);
+        }
+        catch (IOException e) {
             throw new StaticContentNotLoadedException("Cannot download content for file " + file, e);
         }
     }
